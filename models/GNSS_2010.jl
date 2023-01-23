eaf522617aaebc66d54f3387113146406de13a8b
thorek1 <thorek1@users.noreply.github.com>
2023-01-23 14:47:55 +0100

    unexp rates

--- models/GNSS_2010.jl
+++ models/GNSS_2010.jl
@@ -3,9 +3,9 @@ using MacroModelling
 @model GNSS_2010 begin
 	interestPol[0] = 400 * r_ib[0]
 
-	interestH[0] = 400 * exp(r_bh[0])
+	interestH[0] = 400 * r_bh[0]
 
-	interestF[0] = 400 * exp(r_be[0])
+	interestF[0] = 400 * r_be[0]
 
 	inflation[0] = pie[0] * 100
 
@@ -21,7 +21,7 @@ using MacroModelling
 
 	deposits[0] = 100 * D[0]
 
-	interestDep[0] = 400 * exp(r_d[0])
+	interestDep[0] = 400 * r_d[0]
 
 	bankcapital[0] = 100 * K_b[0]
 
@@ -29,27 +29,27 @@ using MacroModelling
 
 	j * exp(ee_j[0]) / exp(h_p[0]) - exp(lam_p[0]) * exp(q_h[0]) + beta_p * exp(lam_p[1]) * exp(q_h[1]) = 0
 
-	exp(lam_p[0]) = beta_p * exp(lam_p[1]) * (1 + exp(r_d[0])) / exp(pie[1])
+	exp(lam_p[0]) = beta_p * exp(lam_p[1]) * (1 + r_d[0]) / exp(pie[1])
 
 	(1 - exp(eps_l[0])) * exp(l_p[0]) + exp(eps_l[0]) * exp(l_p[0]) ^ (1 + phi) / exp(w_p[0]) / exp(lam_p[0]) - exp(pie_wp[0]) * kappa_w * (exp(pie_wp[0]) - exp(pie[-1]) ^ ind_w * piss ^ (1 - ind_w)) + kappa_w * beta_p * exp(lam_p[1]) / exp(lam_p[0]) * (exp(pie_wp[1]) - piss ^ (1 - ind_w) * exp(pie[0]) ^ ind_w) * exp(pie_wp[1]) ^ 2 / exp(pie[1]) = 0
 
 	exp(pie_wp[0]) = exp(pie[0]) * exp(w_p[0]) / exp(w_p[-1])
 
-	exp(c_p[0]) + exp(q_h[0]) * (exp(h_p[0]) - exp(h_p[-1])) + exp(d_p[0]) = exp(l_p[0]) * exp(w_p[0]) + (1 + exp(r_d[-1])) * exp(d_p[-1]) / exp(pie[0]) + exp(J_R[0]) / gamma_p
+	exp(c_p[0]) + exp(q_h[0]) * (exp(h_p[0]) - exp(h_p[-1])) + exp(d_p[0]) = exp(l_p[0]) * exp(w_p[0]) + (1 + r_d[-1]) * exp(d_p[-1]) / exp(pie[0]) + exp(J_R[0]) / gamma_p
 
 	(1 - a_i) * exp(ee_z[0]) * (exp(c_i[0]) - a_i * exp(c_i[-1])) ^ (-1) = exp(lam_i[0])
 
 	j * exp(ee_j[0]) / exp(h_i[0]) - exp(q_h[0]) * exp(lam_i[0]) + exp(q_h[1]) * beta_i * exp(lam_i[1]) + exp(pie[1]) * exp(q_h[1]) * exp(s_i[0]) * exp(m_i[0]) = 0
 
-	exp(lam_i[0]) - beta_i * exp(lam_i[1]) * (1 + exp(r_bh[0])) / exp(pie[1]) = exp(s_i[0]) * (1 + exp(r_bh[0]))
+	exp(lam_i[0]) - beta_i * exp(lam_i[1]) * (1 + r_bh[0]) / exp(pie[1]) = exp(s_i[0]) * (1 + r_bh[0])
 
 	(1 - exp(eps_l[0])) * exp(l_i[0]) + exp(eps_l[0]) * exp(l_i[0]) ^ (1 + phi) / exp(w_i[0]) / exp(lam_i[0]) - exp(pie_wi[0]) * kappa_w * (exp(pie_wi[0]) - exp(pie[-1]) ^ ind_w * piss ^ (1 - ind_w)) + kappa_w * beta_i * exp(lam_i[1]) / exp(lam_i[0]) * (exp(pie_wi[1]) - piss ^ (1 - ind_w) * exp(pie[0]) ^ ind_w) * exp(pie_wi[1]) ^ 2 / exp(pie[1]) = 0
 
 	exp(pie_wi[0]) = exp(pie[0]) * exp(w_i[0]) / exp(w_i[-1])
 
-	exp(c_i[0]) + exp(q_h[0]) * (exp(h_i[0]) - exp(h_i[-1])) + (1 + exp(r_bh[-1])) * exp(b_i[-1]) / exp(pie[0]) = exp(l_i[0]) * exp(w_i[0]) + exp(b_i[0])
+	exp(c_i[0]) + exp(q_h[0]) * (exp(h_i[0]) - exp(h_i[-1])) + (1 + r_bh[-1]) * exp(b_i[-1]) / exp(pie[0]) = exp(l_i[0]) * exp(w_i[0]) + exp(b_i[0])
 
-	(1 + exp(r_bh[0])) * exp(b_i[0]) = exp(pie[1]) * exp(h_i[0]) * exp(q_h[1]) * exp(m_i[0])
+	(1 + r_bh[0]) * exp(b_i[0]) = exp(pie[1]) * exp(h_i[0]) * exp(q_h[1]) * exp(m_i[0])
 
 	exp(K[0]) = (1 - deltak) * exp(K[-1]) + exp(I[0]) * (1 - kappa_i / 2 * (exp(I[0]) * exp(ee_qk[0]) / exp(I[-1]) - 1) ^ 2)
 
@@ -57,25 +57,25 @@ using MacroModelling
 
 	(1 - a_i) * (exp(c_e[0]) - a_i * exp(c_e[-1])) ^ (-1) = exp(lam_e[0])
 
-	(1 - deltak) * exp(pie[1]) * exp(q_k[1]) * exp(s_e[0]) * exp(m_e[0]) + beta_e * exp(lam_e[1]) * ((1 - deltak) * exp(q_k[1]) + exp(r_k[1]) * exp(u[1]) - (eksi_1 * (exp(u[1]) - 1) + eksi_2 / 2 * (exp(u[1]) - 1) ^ 2)) = exp(q_k[0]) * exp(lam_e[0])
+	(1 - deltak) * exp(pie[1]) * exp(q_k[1]) * exp(s_e[0]) * exp(m_e[0]) + beta_e * exp(lam_e[1]) * ((1 - deltak) * exp(q_k[1]) + r_k[1] * exp(u[1]) - (eksi_1 * (exp(u[1]) - 1) + eksi_2 / 2 * (exp(u[1]) - 1) ^ 2)) = exp(q_k[0]) * exp(lam_e[0])
 
 	exp(w_p[0]) = ni * (1 - alpha) * exp(y_e[0]) / (exp(l_pd[0]) * exp(x[0]))
 
 	exp(w_i[0]) = exp(y_e[0]) * (1 - alpha) * (1 - ni) / (exp(x[0]) * exp(l_id[0]))
 
-	exp(lam_e[0]) - exp(s_e[0]) * (1 + exp(r_be[0])) = beta_e * exp(lam_e[1]) * (1 + exp(r_be[0])) / exp(pie[1])
+	exp(lam_e[0]) - exp(s_e[0]) * (1 + r_be[0]) = beta_e * exp(lam_e[1]) * (1 + r_be[0]) / exp(pie[1])
 
-	exp(r_k[0]) = eksi_1 + eksi_2 * (exp(u[0]) - 1)
+	r_k[0] = eksi_1 + eksi_2 * (exp(u[0]) - 1)
 
-	exp(c_e[0]) + (1 + exp(r_be[-1])) * exp(b_ee[-1]) / exp(pie[0]) + exp(w_p[0]) * exp(l_pd[0]) + exp(w_i[0]) * exp(l_id[0]) + exp(q_k[0]) * exp(k_e[0]) + (eksi_1 * (exp(u[0]) - 1) + eksi_2 / 2 * (exp(u[0]) - 1) ^ 2) * exp(k_e[-1]) = exp(y_e[0]) / exp(x[0]) + exp(b_ee[0]) + exp(k_e[-1]) * (1 - deltak) * exp(q_k[0])
+	exp(c_e[0]) + (1 + r_be[-1]) * exp(b_ee[-1]) / exp(pie[0]) + exp(w_p[0]) * exp(l_pd[0]) + exp(w_i[0]) * exp(l_id[0]) + exp(q_k[0]) * exp(k_e[0]) + (eksi_1 * (exp(u[0]) - 1) + eksi_2 / 2 * (exp(u[0]) - 1) ^ 2) * exp(k_e[-1]) = exp(y_e[0]) / exp(x[0]) + exp(b_ee[0]) + exp(k_e[-1]) * (1 - deltak) * exp(q_k[0])
 
 	exp(y_e[0]) = exp(A_e[0]) * (exp(u[0]) * exp(k_e[-1])) ^ alpha * (exp(l_pd[0]) ^ ni * exp(l_id[0]) ^ (1 - ni)) ^ (1 - alpha)
 
-	(1 + exp(r_be[0])) * exp(b_ee[0]) = (1 - deltak) * exp(k_e[0]) * exp(pie[1]) * exp(q_k[1]) * exp(m_e[0])
+	(1 + r_be[0]) * exp(b_ee[0]) = (1 - deltak) * exp(k_e[0]) * exp(pie[1]) * exp(q_k[1]) * exp(m_e[0])
 
-	exp(r_k[0]) = (exp(l_pd[0]) ^ ni * exp(l_id[0]) ^ (1 - ni)) ^ (1 - alpha) * alpha * exp(A_e[0]) * exp(u[0]) ^ (alpha - 1) * exp(k_e[-1]) ^ (alpha - 1) / exp(x[0])
+	r_k[0] = (exp(l_pd[0]) ^ ni * exp(l_id[0]) ^ (1 - ni)) ^ (1 - alpha) * alpha * exp(A_e[0]) * exp(u[0]) ^ (alpha - 1) * exp(k_e[-1]) ^ (alpha - 1) / exp(x[0])
 
-	exp(R_b[0]) = r_ib[0] + ( - kappa_kb) * (exp(K_b[0]) / exp(B[0]) - vi) * (exp(K_b[0]) / exp(B[0])) ^ 2
+	R_b[0] = r_ib[0] + ( - kappa_kb) * (exp(K_b[0]) / exp(B[0]) - vi) * (exp(K_b[0]) / exp(B[0])) ^ 2
 
 	exp(pie[0]) * exp(K_b[0]) = (1 - delta_kb) * exp(K_b[-1]) / exp(eps_K_b[0]) + exp(j_B[-1])
 
@@ -87,13 +87,13 @@ using MacroModelling
 
 	exp(b_h[0]) + exp(b_e[0]) = exp(K_b[0]) + exp(d_b[0])
 
-	kappa_d * beta_p * exp(lam_p[1]) / exp(lam_p[0]) * (exp(r_d[1]) / exp(r_d[0]) - (exp(r_d[0]) / exp(r_d[-1])) ^ ind_d) * (exp(r_d[1]) / exp(r_d[0])) ^ 2 * exp(d_b[1]) / exp(d_b[0]) + exp(mk_d[0]) / (exp(mk_d[0]) - 1) - 1 - r_ib[0] * exp(mk_d[0]) / (exp(mk_d[0]) - 1) / exp(r_d[0]) - exp(r_d[0]) * kappa_d * (exp(r_d[0]) / exp(r_d[-1]) - (exp(r_d[-1]) / exp(r_d[-2])) ^ ind_d) / exp(r_d[-1]) = 0
+	kappa_d * beta_p * exp(lam_p[1]) / exp(lam_p[0]) * (r_d[1] / r_d[0] - (r_d[0] / r_d[-1]) ^ ind_d) * (r_d[1] / r_d[0]) ^ 2 * exp(d_b[1]) / exp(d_b[0]) + exp(mk_d[0]) / (exp(mk_d[0]) - 1) - 1 - r_ib[0] * exp(mk_d[0]) / (exp(mk_d[0]) - 1) / r_d[0] - r_d[0] * kappa_d * (r_d[0] / r_d[-1] - (r_d[-1] / r_d[-2]) ^ ind_d) / r_d[-1] = 0
 
-	beta_p * exp(lam_p[1]) / exp(lam_p[0]) * kappa_be * (exp(r_be[1]) / exp(r_be[0]) - (exp(r_be[0]) / exp(r_be[-1])) ^ ind_be) * (exp(r_be[1]) / exp(r_be[0])) ^ 2 * exp(b_e[1]) / exp(b_e[0]) + 1 - exp(mk_be[0]) / (exp(mk_be[0]) - 1) + exp(R_b[0]) * exp(mk_be[0]) / (exp(mk_be[0]) - 1) / exp(r_be[0]) - exp(r_be[0]) * kappa_be * (exp(r_be[0]) / exp(r_be[-1]) - (exp(r_be[-1]) / exp(r_be[-2])) ^ ind_be) / exp(r_be[-1]) = 0
+	beta_p * exp(lam_p[1]) / exp(lam_p[0]) * kappa_be * (r_be[1] / r_be[0] - (r_be[0] / r_be[-1]) ^ ind_be) * (r_be[1] / r_be[0]) ^ 2 * exp(b_e[1]) / exp(b_e[0]) + 1 - exp(mk_be[0]) / (exp(mk_be[0]) - 1) + R_b[0] * exp(mk_be[0]) / (exp(mk_be[0]) - 1) / r_be[0] - r_be[0] * kappa_be * (r_be[0] / r_be[-1] - (r_be[-1] / r_be[-2]) ^ ind_be) / r_be[-1] = 0
 
-	beta_p * exp(lam_p[1]) / exp(lam_p[0]) * kappa_bh * (exp(r_bh[1]) / exp(r_bh[0]) - (exp(r_bh[0]) / exp(r_bh[-1])) ^ ind_bh) * (exp(r_bh[1]) / exp(r_bh[0])) ^ 2 * exp(b_h[1]) / exp(b_h[0]) + 1 - exp(mk_bh[0]) / (exp(mk_bh[0]) - 1) + exp(R_b[0]) * exp(mk_bh[0]) / (exp(mk_bh[0]) - 1) / exp(r_bh[0]) - exp(r_bh[0]) * kappa_bh * (exp(r_bh[0]) / exp(r_bh[-1]) - (exp(r_bh[-1]) / exp(r_bh[-2])) ^ ind_bh) / exp(r_bh[-1]) = 0
+	beta_p * exp(lam_p[1]) / exp(lam_p[0]) * kappa_bh * (r_bh[1] / r_bh[0] - (r_bh[0] / r_bh[-1]) ^ ind_bh) * (r_bh[1] / r_bh[0]) ^ 2 * exp(b_h[1]) / exp(b_h[0]) + 1 - exp(mk_bh[0]) / (exp(mk_bh[0]) - 1) + R_b[0] * exp(mk_bh[0]) / (exp(mk_bh[0]) - 1) / r_bh[0] - r_bh[0] * kappa_bh * (r_bh[0] / r_bh[-1] - (r_bh[-1] / r_bh[-2]) ^ ind_bh) / r_bh[-1] = 0
 
-	exp(j_B[0]) = exp(r_bh[0]) * exp(b_h[0]) + exp(r_be[0]) * exp(b_e[0]) - exp(r_d[0]) * exp(d_b[0]) - exp(d_b[0]) * exp(r_d[0]) * kappa_d / 2 * (exp(r_d[0]) / exp(r_d[-1]) - 1) ^ 2 - exp(b_e[0]) * exp(r_be[0]) * kappa_be / 2 * (exp(r_be[0]) / exp(r_be[-1]) - 1) ^ 2 - exp(b_h[0]) * exp(r_bh[0]) * kappa_bh / 2 * (exp(r_bh[0]) / exp(r_bh[-1]) - 1) ^ 2 - exp(K_b[0]) * kappa_kb / 2 * (exp(K_b[0]) / exp(B[0]) - vi) ^ 2
+	exp(j_B[0]) = r_bh[0] * exp(b_h[0]) + r_be[0] * exp(b_e[0]) - r_d[0] * exp(d_b[0]) - exp(d_b[0]) * r_d[0] * kappa_d / 2 * (r_d[0] / r_d[-1] - 1) ^ 2 - exp(b_e[0]) * r_be[0] * kappa_be / 2 * (r_be[0] / r_be[-1] - 1) ^ 2 - exp(b_h[0]) * r_bh[0] * kappa_bh / 2 * (r_bh[0] / r_bh[-1] - 1) ^ 2 - exp(K_b[0]) * kappa_kb / 2 * (exp(K_b[0]) / exp(B[0]) - vi) ^ 2
 
 	exp(J_R[0]) = exp(Y[0]) * (1 - 1 / exp(x[0]) - kappa_p / 2 * (exp(pie[0]) - exp(pie[-1]) ^ ind_p * piss ^ (1 - ind_p)) ^ 2)
 
@@ -151,13 +151,13 @@ using MacroModelling
 
 	exp(eps_K_b[0]) = 1 - rho_eps_K_b + rho_eps_K_b * exp(eps_K_b[-1]) + e_eps_K_b[x]
 
-	rr_e[0] = exp(lam_e[0]) - beta_e * exp(lam_e[1]) * (1 + exp(r_be[0])) / exp(pie[1])
+	rr_e[0] = exp(lam_e[0]) - beta_e * exp(lam_e[1]) * (1 + r_be[0]) / exp(pie[1])
 
 	aux1[0] = exp(K_b[0]) / exp(B[0])
 
-	exp(bm[0]) = exp(r_bh[-1]) * exp(b_h[-1]) / (exp(b_h[-1]) + exp(b_e[-1])) + exp(r_be[-1]) * exp(b_e[-1]) / (exp(b_h[-1]) + exp(b_e[-1])) - exp(r_d[-1])
+	exp(bm[0]) = r_bh[-1] * exp(b_h[-1]) / (exp(b_h[-1]) + exp(b_e[-1])) + r_be[-1] * exp(b_e[-1]) / (exp(b_h[-1]) + exp(b_e[-1])) - r_d[-1]
 
-	exp(spr_b[0]) = exp(r_bh[0]) * 0.5 + exp(r_be[0]) * 0.5 - exp(r_d[0])
+	exp(spr_b[0]) = r_bh[0] * 0.5 + r_be[0] * 0.5 - r_d[0]
 end
 
 @parameters GNSS_2010 begin
