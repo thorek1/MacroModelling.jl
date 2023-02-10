19b7adabb47837dccfcea7504e24d828486f4a4f
thorek1 <thorek1@users.noreply.github.com>
2023-02-10 09:03:30 +0100

    SS solves, dynamics not. fix parse of d[1]

--- models/JQ_2012.jl
+++ models/JQ_2012.jl
@@ -1,165 +1,368 @@
+using MacroModelling
+
 @model JQ_2012 begin
-	1 = P[0] * betta * gamma[1] * (c[1] - h * c[0]) ^ (-siggma) / (gamma[0] * (c[0] - h * c[-1]) ^ (-siggma)) * (1 + r[0]) / P[1]
+	w[0] / c[0] ^ siggma - alppha / (1 - n[0]) = 0
 
-	y[0] * 1 / eta[0] * theta / u[0] * (1 - mu[0] * (1 + (d[0] - (d[0])) * 2 * kappa)) - k[-1] * ((1 - xi_bar * (mu[0])) / betta - (1 - delta)) * u[0] ^ psi - (1 + (d[0] - (d[0])) * 2 * kappa) * theta * (1 - eta[0]) / eta[0] / u[0] * chi[0] = 0
+	c[0] ^ (-siggma) = betta * (R[0] - tau) / (1 - tau) * c[1] ^ (-siggma)
 
-	mu[0] * xi[0] + betta * gamma[1] * (c[1] - h * c[0]) ^ (-siggma) / (gamma[0] * (c[0] - h * c[-1]) ^ (-siggma)) * ((1 - delta) * Q[1] + (theta * 1 / eta[1] * y[1] / k[0] - ((1 - xi_bar * (mu[0])) / betta - (1 - delta)) * (u[1] ^ (1 + psi) - 1) / (1 + psi)) / (1 + 2 * kappa * (d[1] - (d[0]))) - theta * 1 / eta[1] * y[1] / k[0] * mu[1] - theta * (1 - eta[1]) / eta[1] / k[0] * chi[1]) - Q[0] = 0
+	w[0] * n[0] + b[-1] - b[0] / R[0] + d[0] - c[0] = 0
 
-	(zeta[0] * (1 - varrho * (invest[0] / invest[-1] - 1) ^ 2) + invest[0] * zeta[0] * (invest[0] / invest[-1] - 1) * varrho * ( - 2) * 1 / invest[-1]) * Q[0] + invest[1] * zeta[1] * varrho * ( - 2) * (invest[1] / invest[0] - 1) * ( - invest[1]) / invest[0] ^ 2 * betta * gamma[1] * (c[1] - h * c[0]) ^ (-siggma) / (gamma[0] * (c[0] - h * c[-1]) ^ (-siggma)) * Q[1] - 1 / (1 + (d[0] - (d[0])) * 2 * kappa) = 0
+	(1 - theta) * z[0] * k[-1] ^ theta * n[0] ^ (-theta) = w[0] * 1 / (1 - mu[0] * (1 + 2 * kappa * (d[0] - d[ss])))
 
-	k[0] = invest[0] * zeta[0] * (1 - varrho * (invest[0] / invest[-1] - 1) ^ 2) + (1 - delta) * k[-1]
+	betta * (c[0] / c[1]) ^ siggma * (1 + 2 * kappa * (d[0] - d[ss])) / (1 + 2 * kappa * (d[1] - d[ss])) * (1 - delta + theta * (1 - (1 + 2 * kappa * (d[1] - d[ss])) * mu[1]) * z[1] * k[0] ^ (theta - 1) * n[1] ^ (1 - theta)) + (1 + 2 * kappa * (d[0] - d[ss])) * mu[0] * xi[0] = 1
 
-	betta * omega * wopthat[1] + chat[-1] * ( - (epsilon * (upsilon_bar - 1) * (1 - betta * omega) / (upsilon_bar + epsilon * (upsilon_bar - 1)) * h * siggma / (1 - h))) + chat[0] * epsilon * (upsilon_bar - 1) * (1 - betta * omega) / (upsilon_bar + epsilon * (upsilon_bar - 1)) * siggma / (1 - h) + epsilon * (upsilon_bar - 1) * (1 - betta * omega) / (upsilon_bar + epsilon * (upsilon_bar - 1)) * (log(P[0]) - log((P[0]))) + epsilon * (upsilon_bar - 1) * (1 - betta * omega) / (upsilon_bar + epsilon * (upsilon_bar - 1)) * upsilonhat[0] + (log(n[0]) - log((n[0]))) * epsilon * (upsilon_bar - 1) * (1 - betta * omega) / (upsilon_bar + epsilon * (upsilon_bar - 1)) / epsilon + What[0] * upsilon_bar * epsilon * (upsilon_bar - 1) * (1 - betta * omega) / (upsilon_bar + epsilon * (upsilon_bar - 1)) / (epsilon * (upsilon_bar - 1)) - wopthat[0] = 0
+	(1 + 2 * kappa * (d[0] - d[ss])) / (1 + 2 * kappa * (d[1] - d[ss])) * (c[0] / c[1]) ^ siggma * betta * R[0] + (1 + 2 * kappa * (d[0] - d[ss])) * mu[0] * xi[0] * R[0] * (1 - tau) / (R[0] - tau) = 1
 
-	W[0] = (omega * W[-1] ^ (1 / (1 - upsilon[0])) + (1 - omega) * w_opt[0] ^ (1 / (1 - upsilon[0]))) ^ (1 - upsilon[0])
+	b[0] / R[0] + k[-1] * (1 - delta) + z[0] * k[-1] ^ theta * n[0] ^ (1 - theta) - w[0] * n[0] - b[-1] - k[0] - (d[0] + kappa * (d[0] - d[ss]) ^ 2) = 0
 
-	y[0] * 1 / eta[0] * (1 - theta) / n[0] * (1 - mu[0] * (1 + (d[0] - (d[0])) * 2 * kappa)) - W[0] / P[0] - (1 - theta) * (1 - eta[0]) / eta[0] / n[0] * (1 + (d[0] - (d[0])) * 2 * kappa) * chi[0] = 0
+	xi[0] * (k[0] - b[0] * (1 - tau) / (R[0] - tau)) = z[0] * k[-1] ^ theta * n[0] ^ (1 - theta)
 
-	P[0] * betta * gamma[1] * (c[1] - h * c[0]) ^ (-siggma) / (gamma[0] * (c[0] - h * c[-1]) ^ (-siggma)) * R[0] * (1 + (d[0] - (d[0])) * 2 * kappa) / (1 + 2 * kappa * (d[1] - (d[0]))) / P[1] + R[0] * (1 + (d[0] - (d[0])) * 2 * kappa) * mu[0] * xi[0] / (1 + r[0]) = 1
+	log(z[0] / z[ss]) = A11 * log(z[-1] / z[ss]) + A12 * log(xi[-1] / xi_bar) + eps_z[x]
 
-	P[0] * (y[0] * phi * (P[0] / P[-1] - 1) / P[-1] + y[1] * phi * (P[1] / P[0] - 1) * ( - P[1]) / P[0] ^ 2 * betta * gamma[1] * (c[1] - h * c[0]) ^ (-siggma) / (gamma[0] * (c[0] - h * c[-1]) ^ (-siggma)) * (1 + (d[0] - (d[0])) * 2 * kappa) / (1 + 2 * kappa * (d[1] - (d[0])))) - (1 + (d[0] - (d[0])) * 2 * kappa) * chi[0] = 0
+	log(xi[0] / xi_bar) = log(z[-1] / z[ss]) * A21 + log(xi[-1] / xi_bar) * A22 + eps_xi[x]
 
-	V[0] = d[0] + betta * gamma[1] * (c[1] - h * c[0]) ^ (-siggma) / (gamma[0] * (c[0] - h * c[-1]) ^ (-siggma)) * V[1]
+	y[0] = z[0] * k[-1] ^ theta * n[0] ^ (1 - theta)
 
-	xi[0] * (k[0] - b[0] / (P[0] * (1 + r[0]))) = y[0]
+	invest[0] = k[0] - k[-1] * (1 - delta)
 
-	b[0] / R[0] + P[0] * (y[0] - k[-1] * ((1 - xi_bar * (mu[0])) / betta - (1 - delta)) * (u[0] ^ (1 + psi) - 1) / (1 + psi)) - b[-1] - n[0] * W[0] - P[0] * y[0] * phi / 2 * (P[0] / P[-1] - 1) ^ 2 - (d[0] + kappa * (d[0] - (d[0])) ^ 2) * P[0] - invest[0] * P[0] = 0
-
-	b[-1] + n[0] * W[0] + d[0] * P[0] - b[0] / (1 + r[0]) - c[0] * P[0] - T[0] = 0
-
-	P[0] * G[0] + b[0] * (1 / R[0] - 1 / (1 + r[0])) - T[0] = 0
-
-	rho_R * (r[-1] - (r[0])) + (1 - rho_R) * nu_1 * (P[0] - P[-1]) + ((1 - rho_R) * nu_2 + nu_3) * (y[0] - (y[0])) - nu_3 * (y[-1] - (y[0])) + var_sigma[0] - (r[0] - (r[0])) = 0
-
-	y[0] = exp(z[0]) * (u[0] * k[-1]) ^ theta * n[0] ^ (1 - theta)
-
-	byhat[0] = (b[-1] / (1 + r[-1]) - b[0] / (1 + r[0])) / (y[0] * P[0])
+	v[0] = d[0] + c[0] * betta / c[1] * v[1]
 
 	r[0] = (R[0] - tau) / (1 - tau) - 1
 
-	yhat[0] = log(y[0]) - log((y[0]))
+	# yhat[0] = log(y[0]) - log(y[ss])
 
-	chat[0] = log(c[0]) - log((c[0]))
+	# chat[0] = log(c[0]) - log(c[ss])
 
-	ihat[0] = log(invest[0]) - log((invest[0]))
+	# ihat[0] = log(invest[0]) - log(invest[ss])
 
-	nhat[0] = log(n[0]) - log((n[0]))
+	# nhat[0] = log(n[0]) - log(n[ss])
 
-	muhat[0] = log(mu[0]) - log((mu[0]))
+	# muhat[0] = log(mu[0]) - log(mu[ss])
 
-	upsilonhat[0] = log(upsilon[0]) - log(upsilon_bar)
+	# byhat[0] = (b[-1] / (1 + r[-1]) - b[0] / (1 + r[0])) / y[0]
 
-	wopthat[0] = log(w_opt[0]) - log((w_opt[0]))
+	# dyhat[0] = d[0] / y[0]
 
-	What[0] = log(W[0]) - log((W[0]))
-
-	dyhat[0] = d[0] / y[0]
-
-	vyhat[0] = log(V[0] / (k[-1] - b[-1])) - log((V[0]) / ((k[0]) - (b[0])))
-
-	z[0] = rho_z * z[-1] + eps_z[x]
-
-	log(zeta[0]) = rho_zeta * log(zeta[-1]) + eps_zeta[x]
-
-	log(gamma[0]) = rho_gamma * log(gamma[-1]) + eps_gamma[x]
-
-	log(eta[0] / eta_bar) = rho_eta * log(eta[-1] / eta_bar) + eps_eta[x]
-
-	log(upsilon[0] / upsilon_bar) = rho_upsilon * log(upsilon[-1] / upsilon_bar) + eps_upsilon[x]
-
-	log(G[0] / G_bar) = rho_G * log(G[-1] / G_bar) + rho_gz * (z[0] - z[-1]) + eps_G[x]
-
-	var_sigma[0] = rho_varsigma * var_sigma[-1] + eps_varsigma[x]
-
-	log(xi[0] / xi_bar) = rho_xi * log(xi[-1] / xi_bar) + eps_xi[x]
-
-	# y_obs[0] = log(y[0]) - log(y[-1])
-
-	# c_obs[0] = log(c[0]) - log(c[-1])
-
-	# invest_obs[0] = log(invest[0]) - log(invest[-1])
-
-	# pi_obs[0] = log(P[0]) - log(P[-1])
-
-	# r_obs[0] = r[0] - (r[0])
-
-	# n_obs[0] = log(n[0]) - log(n[-1])
-
-	# W_obs[0] = log(W[0] / P[0]) - log(W[-1] / P[-1])
-
-	# debt_repurchase_obs[0] = byhat[0]
+	# vyhat[0] = log(v[0] / (k[-1] - b[-1])) - log((v[0] / (k[-1] - b[-1])))
 
 end
 
 
 @parameters JQ_2012 begin
-	betta = 0.9825
 
-	tau = 0.35
+	n[ss] = .3 | alppha
 
-	alppha = 16.736
+	# BY_ratio = 3.36
+
+	b[ss] / (1 + r[ss]) / y[ss] = 3.36 | xi_bar
+
+	# xi_bar = .163
+
+	z[ss] = 1 | betta
+
+	# betta = 0.9825
+
+	siggma = 1
 
 	theta = 0.36
 
 	delta = 0.025
 
-	# xi_bar = 0.199
+	tau = 0.35
 
-	BY_ratio = 3.36
+	kappa = 0.146
 
-	b[ss] / (1 + r[ss]) / y[ss] = BY_ratio | xi_bar
+	kappa_store = kappa
 
-	# G[ss] = 0.179 | G_bar
+	sigma_xi = 0.0098
 
-	G[ss] = 0.18 * y[ss] | G_bar
+	sigma_z = 0.0045
 
-	# GY_ratio = 0.18
+	covariance_z_xi =0
 
-	siggma = 1.09
+	A11 = 0.9457
 
-	epsilon = 1.761
+	A12 = -0.0091
 
-	h = 0.608
+	A21 = 0.0321
 
-	omega = 0.278
+	A22 = 0.9703
 
-	phi = 0.031
+	b > .1
 
-	varrho = 0.021
-
-	psi = 0.815
-
-	kappa = 0.426
-
-	eta[ss] = 1.137 | eta_bar
-
-	upsilon[ss] = 1.025 | upsilon_bar
-
-	rho_z = 0.902
-
-	rho_zeta = 0.922
-
-	rho_gamma = 0.794
-
-	rho_eta = 0.906
-
-	rho_upsilon = 0.627
-
-	rho_G = 0.955
-
-	rho_varsigma = 0.203
-
-	rho_xi = 0.969
-
-	rho_gz = 0.509
-
-	rho_R = 0.745
-
-	nu_1 = 2.410
-
-	nu_2 = 0
-
-	nu_3 = 0.121
+	-100 < xi < 100
 
 end
+
+plot(JQ_2012)
+get_solution(JQ_2012)
+
+
+JQ_2012.SS_solve_func
+
+
+using Optimization, OptimizationNLopt, NLboxsolve
+# get_SS(m,verbose = true)
+𝓂 = JQ_2012
+params = 𝓂.parameter_values
+fail_fast_solvers_only = false
+verbose = true
+
+
+
+siggma = params[1]
+theta = params[2]
+delta = params[3]
+tau = params[4]
+kappa = params[5]
+A21 = params[11]
+A22 = params[12]
+#= /Users/thorekockerols/GitHub/MacroModelling.jl/src/MacroModelling.jl:738 =#
+#= /Users/thorekockerols/GitHub/MacroModelling.jl/src/MacroModelling.jl:739 =#
+kappa_store = kappa
+#= /Users/thorekockerols/GitHub/MacroModelling.jl/src/MacroModelling.jl:740 =#
+NSSS_solver_cache_tmp = []
+#= /Users/thorekockerols/GitHub/MacroModelling.jl/src/MacroModelling.jl:741 =#
+solution_error = 0.0
+#= /Users/thorekockerols/GitHub/MacroModelling.jl/src/MacroModelling.jl:742 =#
+nonnegativity_auxilliary₄ = max(eps(), 1)
+nonnegativity_auxilliary₃ = max(eps(), 1)
+nonnegativity_auxilliary₅ = max(eps(), 1)
+nonnegativity_auxilliary₇ = max(eps(), 1)
+lbs = [1.1920928955078125e-7, 1.1920928955078125e-7, -100.0, -1.0e12]
+ubs = [1.0e12, 1.0e12, 100.0, 1.0e12]
+inits = max.(lbs, min.(ubs, fill(.9,length(ubs))))
+block_solver_RD = block_solver_AD([A21, A22], 1, 𝓂.ss_solve_blocks[1], inits, lbs, ubs, fail_fast_solvers_only = fail_fast_solvers_only, verbose = verbose)
+solution = block_solver_RD([A21, A22])
+solution_error += sum(abs2, (𝓂.ss_solve_blocks[1])([A21, A22], solution, 0))
+sol = solution
+nonnegativity_auxilliary₆ = sol[1]
+nonnegativity_auxilliary₈ = sol[2]
+xi = sol[3]
+xi_bar = sol[4]
+NSSS_solver_cache_tmp = [NSSS_solver_cache_tmp..., if typeof(sol) == Vector{Float64}
+			sol
+		else
+			ℱ.value.(sol)
+		end]
+z = 1
+nonnegativity_auxilliary₂ = max(eps(), 1)
+n = 0.3
+nonnegativity_auxilliary₁ = max(eps(), 1)
+lbs = [-1.0e12, 0.10000000000000023, -1.0e12, 1.1920928955078125e-7, -1.0e12, -1.0e12, -1.0e12]
+ubs = [1.0e12, 1.0e12, 1.0e12, 1.0e12, 1.0e12, 1.0e12, 1.0e12]
+inits = max.(lbs, min.(ubs, fill(.9,length(ubs))))
+block_solver_RD = block_solver_AD([siggma, theta, delta, tau, kappa, xi], 2, 𝓂.ss_solve_blocks[2], inits, lbs, ubs, fail_fast_solvers_only = fail_fast_solvers_only, verbose = verbose)
+solution = block_solver_RD([siggma, theta, delta, tau, kappa, xi])
+solution_error += sum(abs2, (𝓂.ss_solve_blocks[2])([siggma, theta, delta, tau, kappa, xi], solution, 0))
+sol = solution
+R = sol[1]
+b = sol[2]
+betta = sol[3]
+k = sol[4]
+mu = sol[5]
+r = sol[6]
+y = sol[7]
+NSSS_solver_cache_tmp = [NSSS_solver_cache_tmp..., if typeof(sol) == Vector{Float64}
+			sol
+		else
+			ℱ.value.(sol)
+		end]
+invest = delta * k
+w = (3.33333333333333k) ^ theta * (((((2.0 * kappa * mu * theta - 2.0 * kappa * mu) + mu * theta) - mu) - theta) + 1.0)
+d = ((-b - delta * k) - 0.3w) + b / R + (0.3 * (10.0k) ^ theta) / 3.0 ^ theta
+c = (b + d + 0.3w) - b / R
+alppha = (0.7w) / c ^ siggma
+v = -d / (betta - 1)
+
+
+
+
+var_past = @ignore_derivatives setdiff(𝓂.var_past,𝓂.nonnegativity_auxilliary_vars)
+var_present = @ignore_derivatives setdiff(𝓂.var_present,𝓂.nonnegativity_auxilliary_vars)
+var_future = @ignore_derivatives setdiff(𝓂.var_future,𝓂.nonnegativity_auxilliary_vars)
+
+SS = SS_and_pars[1:end - length(𝓂.calibration_equations)]
+calibrated_parameters = SS_and_pars[(end - length(𝓂.calibration_equations)+1):end]
+# par = ComponentVector(vcat(parameters,calibrated_parameters),Axis(vcat(𝓂.parameters,𝓂.calibration_equations_parameters)))
+par = vcat(parameters,calibrated_parameters)
+
+past_idx = @ignore_derivatives [indexin(sort([var_past; map(x -> Symbol(replace(string(x), r"ᴸ⁽⁻[⁰¹²³⁴⁵⁶⁷⁸⁹]+⁾|ᴸ⁽[⁰¹²³⁴⁵⁶⁷⁸⁹]+⁾" => "")),  union(𝓂.aux_past,𝓂.exo_past))]), sort(union(𝓂.var,𝓂.exo_present)))...]
+SS_past =       length(past_idx) > 0 ? SS[past_idx] : zeros(0) #; zeros(length(𝓂.exo_past))...]
+
+present_idx = @ignore_derivatives [indexin(sort([var_present; map(x -> Symbol(replace(string(x), r"ᴸ⁽⁻[⁰¹²³⁴⁵⁶⁷⁸⁹]+⁾|ᴸ⁽[⁰¹²³⁴⁵⁶⁷⁸⁹]+⁾" => "")),  union(𝓂.aux_present,𝓂.exo_present))]), sort(union(𝓂.var,𝓂.exo_present)))...]
+SS_present =    length(present_idx) > 0 ? SS[present_idx] : zeros(0)#; zeros(length(𝓂.exo_present))...]
+
+future_idx = @ignore_derivatives [indexin(sort([var_future; map(x -> Symbol(replace(string(x), r"ᴸ⁽⁻[⁰¹²³⁴⁵⁶⁷⁸⁹]+⁾|ᴸ⁽[⁰¹²³⁴⁵⁶⁷⁸⁹]+⁾" => "")),  union(𝓂.aux_future,𝓂.exo_future))]), sort(union(𝓂.var,𝓂.exo_present)))...]
+SS_future =     length(future_idx) > 0 ? SS[future_idx] : zeros(0)#; zeros(length(𝓂.exo_future))...]
+
+shocks_ss = zeros(length(𝓂.exo))
+
+# return ℱ.jacobian(x -> 𝓂.model_function(x, par, SS), [SS_future; SS_present; SS_past; shocks_ss])#, SS_and_pars
+return Matrix(𝓂.model_jacobian([SS_future; SS_present; SS_past; shocks_ss], par, SS))
+
+
+
+
+using MacroTools: postwalk
+
+# create data containers
+exo = Set()
+aux = Set()
+var = Set()
+par = Set()
+ss = Set()
+
+dyn_ss_past = Set()
+dyn_ss_future = Set()
+dyn_ss_present = Set()
+
+var_future = Set()
+var_present = Set()
+var_past = Set()
+
+aux_future = Set()
+aux_present = Set()
+aux_past = Set()
+
+parameters = []
+parameter_values = Vector{Float64}(undef,0)
+
+exo_list = []
+ss_list = []
+
+ss_calib_list = []
+par_calib_list = []
+var_list = []
+dynamic_variables_list = []
+dynamic_variables_future_list = []
+# var_redundant_list = nothing
+# var_redundant_calib_list = nothing
+# var_solved_list = nothing
+# var_solved_calib_list = nothing
+# var_remaining_list = []
+par_list = []
+var_future_list = []
+var_present_list = []
+var_past_list = []
+
+dyn_exo_future_list = []
+dyn_exo_present_list = []
+dyn_exo_past_list = []
+
+# dyn_aux_future_list = []
+# dyn_aux_present_list = []
+# dyn_aux_past_list = []
+
+exo_future = Set()
+exo_present = Set()
+exo_past = Set()
+
+dyn_shift_var_future_list = []
+dyn_shift_var_present_list = []
+dyn_shift_var_past_list = []
+
+dyn_shift2_var_past_list = []
+
+dyn_var_future_list = []
+dyn_var_present_list = []
+dyn_var_past_list = []
+dyn_exo_list = []
+dyn_ss_list = []
+
+solved_vars = [] 
+solved_vals = []
+
+non_linear_solved_vars = []
+non_linear_solved_vals = []
+
+# solved_sub_vals = []
+# solved_sub_values = []
+ss_solve_blocks = []
+# ss_solve_blocks_no_transform = []
+ss_solve_blocks_optim = []
+# SS_init_guess = Vector{Float64}(undef,0)
+# NSSS_solver_cache = CircularBuffer{Vector{Vector{Float64}}}(500)
+SS_solve_func = nothing
+nonlinear_solution_helper = nothing
+SS_dependencies = nothing
+
+ss_equations = []
+equations = []
+calibration_equations = []
+calibration_equations_parameters = []
+
+bounds⁺ = Set()
+
+bounded_vars = []
+lower_bounds = []
+upper_bounds = []
+
+t_future_equations = []
+t_past_equations = []
+t_present_equations = []
+dyn_equations = []
+dyn_equations_future = []
+
+
+ex = :(betta * (c[0] / c[1]) ^ siggma * (1 + 2 * kappa * (d[0] - d[ss])) / (1 + 2 * kappa * (d[1] - d[ss])) * (1 - delta + theta * (1 - (1 + 2 * kappa * (d[1] - d[ss])) * mu[1]) * z[1] * k[0] ^ (theta - 1) * n[1] ^ (1 - theta)) + (1 + 2 * kappa * (d[0] - d[ss])) * mu[0] * xi[0] = 1)
+
+
+# label all variables parameters and exogenous variables and timings across all equations
+postwalk(x -> 
+		x isa Expr ? 
+			x.head == :ref ? 
+				x.args[2] isa Int ? 
+					x.args[2] == 0 ? 
+						push!(var_present,x.args[1]) : 
+					x.args[2] > 1 ? 
+						begin
+							time_idx = x.args[2]
+
+							while time_idx > 1
+								push!(aux_future,Symbol(string(x.args[1]) * "ᴸ⁽" * super(string(time_idx - 1)) * "⁾"))
+								push!(aux_present,Symbol(string(x.args[1]) * "ᴸ⁽" * super(string(time_idx - 1)) * "⁾"))
+								
+								time_idx -= 1
+							end
+
+							push!(var_future,x.args[1])
+						end : 
+					1 >= x.args[2] > 0 ? 
+						push!(var_future,x.args[1]) : 
+					-1 <= x.args[2] < 0 ? 
+						push!(var_past,x.args[1]) : 
+					x.args[2] < -1 ? 
+						begin
+							time_idx = x.args[2]
+
+							while time_idx < -1
+								push!(aux_past,Symbol(string(x.args[1]) * "ᴸ⁽⁻" * super(string(time_idx + 1)) * "⁾"))
+								push!(aux_present,Symbol(string(x.args[1]) * "ᴸ⁽⁻" * super(string(time_idx + 1)) * "⁾"))
+
+								time_idx += 1
+							end
+
+							push!(var_past,x.args[1])
+						end : 
+					x :
+				# issubset([x.args[2]],[:x :ex :exo :exogenous]) ?
+				occursin(r"^(x|ex|exo|exogenous){1}$"i,string(x.args[2])) ?
+					push!(exo,x.args[1]) :
+				# issubset([x.args[2]],[:ss :SS :ℳ :StSt :steady :steadystate :steady_state :Steady_State]) ?
+				occursin(r"^(ss|stst|steady|steadystate|steady_state){1}$"i,string(x.args[2])) ?
+					push!(ss,x.args[1]) :
+				x : 
+			x.head == :call ? 
+				for i in 2:length(x.args)
+					x.args[i] isa Symbol ? 
+						occursin(r"^(ss|stst|steady|steadystate|steady_state|x|ex|exo|exogenous){1}$"i,string(x.args[i])) ? 
+							x :
+						push!(par,x.args[i]) : 
+					x
+				end : 
+			x :
+		x,
+ex)
+
 
