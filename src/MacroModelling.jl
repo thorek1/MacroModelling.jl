f94e5cd01af802642053ab9c6e112be9e74a43a8
thorek1 <thorek1@users.noreply.github.com>
2023-02-28 18:19:39 +0100

    fix ss_solve_block

--- src/MacroModelling.jl
+++ src/MacroModelling.jl
@@ -950,7 +950,7 @@ block_solver_AD(parameters_and_solved_vars::Vector{<: Number},
                                                             starting_points = starting_points,
                                                             fail_fast_solvers_only = fail_fast_solvers_only,
                                                             verbose = verbose)[1],  
-                                        (x,y) -> ss_solve_blocks(x,y,0))
+                                        (x,y) -> ss_solve_blocks(x,y,0,lbs,ubs))
 
 function block_solver(parameters_and_solved_vars::Vector{Float64}, 
                         n_block::Int, 
