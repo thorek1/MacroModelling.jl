911b0124cbae5c7996cf3e43ee7aec609697ab89
thorek1 <thorek1@users.noreply.github.com>
2023-06-01 00:06:08 +0200

    test museinference

---
+++ test/filter_free_muse_inference.jl
@@ -0,0 +1,252 @@
+using MacroModelling
+import Turing, StatsPlots, Random, Statistics
+import LinearAlgebra as ℒ
+
+@model RBC begin
+    1 / (- k[0]  + (1 - δ ) * k[-1] + (exp(z[-1]) * k[-1]^α)) = (β   / (- k[+1]  + (1 - δ) * k[0] +(exp(z[0]) * k[0]^α))) * (α* exp(z[0]) * k[0] ^(α - 1) + (1 - δ))  ;
+    #    1 / c[0] - (β / c[1]) * (α * exp(z[1]) * k[1]^(α - 1) + (1 - δ)) =0
+    #    q[0] = exp(z[0]) * k[0]^α 
+    z[0] =  ρ * z[-1] - σ* EPSz[x]
+end
+
+@parameters RBC verbose = true begin 
+    σ = 0.01
+    α = 0.25
+    β = 0.95
+    ρ = 0.2
+    δ = 0.02
+    γ = 1.
+end
+
+# draw shocks
+periods = 100
+shocks = randn(1,periods)
+shocks /= Statistics.std(shocks)  # antithetic shocks
+shocks .-= Statistics.mean(shocks) # antithetic shocks
+
+# get simulation
+simulated_data = get_irf(RBC,shocks = shocks, periods = 0, levels = true)#(:k,:,:) |>collect
+
+# plot simulation
+plot_irf(RBC,shocks = shocks, periods = 0)
+StatsPlots.plot(shocks')
+
+
+# function ϵ_loss(Δ; ϵ = .01, p = 2)
+#     abs(Δ) > ϵ ? abs(Δ)^p : 0
+# end
+
+# define loglikelihood model
+Turing.@model function loglikelihood_scaling_function(m, data, observables, Ω)
+    #σ     ~ MacroModelling.Beta(0.01, 0.02, μσ = true)
+    # α     ~ MacroModelling.Beta(0.25, 0.15, 0.1, .4, μσ = true)
+    # β     ~ MacroModelling.Beta(0.95, 0.05, .9, .9999, μσ = true)
+    #ρ     ~ MacroModelling.Beta(0.2, 0.1, μσ = true)
+    δ     ~ MacroModelling.Normal(0.02, 0.05)
+    # γ     ~ Turing.Normal(1, 0.05)
+    σ     ~ MacroModelling.Normal(0.01, 0.05)
+
+    # α ~ Turing.Uniform(0.15, 0.45)
+    # β ~ Turing.Uniform(0.92, 0.9999)
+    # δ ~ Turing.Uniform(0.0, 1.0)
+    # σ ~ Turing.Uniform(0.0, 1.0)
+    # ρ ~ Turing.Uniform(0.0, 1.0)
+    # γ ~ Turing.Uniform(0.0, 1.5)
+
+    α = 0.25
+    β = 0.95
+    # δ = 0.02
+    # σ = 0.01
+    ρ = 0.2
+    γ = 1.
+
+    algorithm = :first_order
+    parameters = [σ, α, β, ρ, δ, γ]
+    shock_distribution = Turing.Normal()
+
+    # Turing.@addlogprob! calculate_kalman_filter_loglikelihood(m, data(observables), observables; parameters = parameters)
+
+    solution = get_solution(m, parameters, algorithm = algorithm)
+
+    if solution[end] != true
+        return Turing.@addlogprob! Inf
+    end
+    # draw_shocks(m)
+    x0 ~ Turing.filldist(Turing.Normal(), m.timings.nPast_not_future_and_mixed) # Initial conditions 
+    
+    calculate_covariance_ = calculate_covariance_AD(solution[2], T = m.timings, subset_indices = collect(1:m.timings.nVars))
+
+    long_run_covariance = calculate_covariance_(solution[2])
+    
+    initial_conditions = long_run_covariance * x0
+    # initial_conditions = x0
+
+    𝐒₁ = hcat(solution[2][:,1:m.timings.nPast_not_future_and_mixed], zeros(m.timings.nVars), solution[2][:,m.timings.nPast_not_future_and_mixed+1:end])
+
+    ϵ_draw ~ Turing.filldist(shock_distribution, m.timings.nExo * size(data, 2))
+
+    ϵ = reshape(ϵ_draw, m.timings.nExo, size(data, 2))
+
+    state = zeros(typeof(initial_conditions[1]), m.timings.nVars, size(data, 2))
+
+    aug_state = [initial_conditions
+                1 
+                ϵ[:,1]]
+
+    state[:,1] .=  𝐒₁ * aug_state# + solution[3] * ℒ.kron(aug_state, aug_state) / 2 
+
+    for t in 2:size(data, 2)
+        aug_state = [state[m.timings.past_not_future_and_mixed_idx,t-1]
+                    1 
+                    ϵ[:,t]]
+
+        state[:,t] .=  𝐒₁ * aug_state# + solution[3] * ℒ.kron(aug_state, aug_state) / 2 
+    end
+
+    observables_index = sort(indexin(observables, m.timings.var))
+    
+    # state_deviations = data - state[observables_index,:] .- solution[1][observables_index...]
+
+    states ~ Turing.MvNormal(vec(state[observables_index,:] .+ solution[1][observables_index...]), Ω * ℒ.I(length(state[observables_index,:])))
+
+    # Turing.@addlogprob! sum([Turing.logpdf(Turing.MvNormal(Ω * ℒ.I(size(data,1))), state_deviations[:,t]) for t in 1:size(data, 2)])
+end
+
+Ω = 1e-0#eps()
+# loglikelihood_scaling = loglikelihood_scaling_function(RBC, simulated_data(:,:,:Shock_matrix), [:k], Ω) # Kalman
+loglikelihood_scaling = loglikelihood_scaling_function(RBC, collect(simulated_data(:k,:,:Shock_matrix))', [:k], Ω) # Filter free
+
+n_samples = 1000
+
+# solution = get_solution(RBC, RBC.parameter_values, algorithm = :first_order)[1]
+
+# simulated_data(:k,:,:Shock_matrix) .- solution[1][observables_index...]
+# using AdvancedHMC
+# D = 18
+
+# n_samples, n_adapts = 1_000, 300
+# metric = DiagEuclideanMetric(D)
+# hamiltonian = Hamiltonian(metric, logprob,
+#     ForwardDiff)
+
+# integrator = Leapfrog(find_good_stepsize(hamiltonian,
+#     initial_theta))
+
+# proposal = NUTS{MultinomialTS, GeneralisedNoUTurn}(integrator)
+# adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric),
+#     StepSizeAdaptor(0.8, integrator))
+
+# samples, stats = sample(hamiltonian, proposal, initial_theta, n_samples, adaptor, n_adapts; progress=true)
+
+samps = Turing.sample(loglikelihood_scaling, Turing.NUTS(), n_samples, progress = true)#, init_params = sol
+# samps = Turing.sample(loglikelihood_scaling, Turing.HMCDA(1000,.65,.75;init_ϵ = .05), n_samples, progress = true)#, init_params = sol)
+# samps = Turing.sample(loglikelihood_scaling, Turing.NUTS(1000, .65; init_ϵ = .01), n_samples, progress = true)#, init_params = sol)
+
+
+using MuseInference, StatsPlots
+import Turing
+using AbstractDifferentiation
+
+model = loglikelihood_scaling | (states = vec(simulated_data(:k,:,:Shock_matrix)'),)
+prob = MuseInference.TuringMuseProblem(model, params=(:δ, :σ))
+
+result = muse(prob, (δ=0.2, σ=0.01,))
+
+# get solution
+result = muse(prob, (δ=0.5, σ=0.1))
+
+# filter free generates good parameter estimates but does not necessarily nail the latent states and shocks
+# std of MvNormal determines speed, convergence (too small no convergence), accuracy (smaller value will get more info on latent shocks and shock related parameters [shock size and persistence])
+# the logic behind it appears to be: ideally the data is perfectly matched so you want the MvNormal to have a very small std but then the sampler will focus on matching the data since thats where the gradients are very large. the gradients regarding parameter priors are too small to have any influence and the sampler gets stuck if parameters do change because the gradients to match the data dominate.
+
+interval = -.01:.0001:.01
+interval = -1:.01:1
+
+StatsPlots.plot(x->abs(x)^4,interval)
+StatsPlots.plot!(x->abs(x)^3,interval)
+StatsPlots.plot!(x->abs(x)^2,interval)
+
+
+
+StatsPlots.plot(x->4*x^3,interval)
+StatsPlots.plot!(x->3*x*abs(x),interval)
+StatsPlots.plot!(x->2*x,interval)
+
+interval = -.01:.0001:.01
+
+StatsPlots.plot(x->abs(x)^4,interval)
+StatsPlots.plot!(x->abs(x)^3,interval)
+StatsPlots.plot!(x->abs(x)^2,interval)
+
+
+
+StatsPlots.plot(samps)
+
+#Plot true and estimated latents to see how well we backed them out
+estimated_parameters = Turing.describe(samps)[1].nt.parameters
+estimated_parameters_indices = indexin([Symbol("ϵ_draw[$a]") for a in 1:periods], estimated_parameters)
+estimated_means = Turing.describe(samps)[1].nt.mean
+estimated_std = Turing.describe(samps)[1].nt.std
+
+
+StatsPlots.plot(estimated_means[estimated_parameters_indices],
+                ribbon = 1.96 * estimated_std[estimated_parameters_indices], 
+                label = "Posterior mean", 
+                title = "First-Order Joint: Estimated Latents")
+StatsPlots.plot!(shocks', label = "True values")
+
+
+
+
+
+# testing functions
+
+function calculate_filter_free_llh(m, parameters, data, observables; algorithm = :first_order, shock_distribution = Turing.Normal(), Ω::Float64 = sqrt(eps()))
+    solution = get_solution(m, parameters, algorithm = algorithm)
+
+    if solution[end] != true
+        return Turing.@addlogprob! Inf
+    end
+    
+    x0 ~ Turing.filldist(Turing.Normal(), m.timings.nPast_not_future_and_mixed) # Initial conditions 
+    
+    calculate_covariance_ = calculate_covariance_AD(solution[2], T = m.timings, subset_indices = collect(1:m.timings.nVars))
+
+    long_run_covariance = calculate_covariance_(solution[2])
+    
+    initial_conditions = long_run_covariance * x0
+    # initial_conditions = x0
+
+    𝐒₁ = hcat(solution[2][:,1:m.timings.nPast_not_future_and_mixed], zeros(m.timings.nVars), solution[2][:,m.timings.nPast_not_future_and_mixed+1:end])
+
+    ϵ_draw ~ Turing.filldist(shock_distribution, m.timings.nExo * size(data, 2)) #Shocks are t-distributed!
+
+    ϵ = reshape(ϵ_draw, m.timings.nExo, size(data, 2))
+
+    state = zeros(typeof(initial_conditions[1]), m.timings.nVars, size(data, 2))
+
+    aug_state = [initial_conditions
+                1 
+                ϵ[:,1]]
+
+    state[:,1] .=  𝐒₁ * aug_state# + solution[3] * ℒ.kron(aug_state, aug_state) / 2 
+
+    for t in 2:size(data, 2)
+        aug_state = [state[m.timings.past_not_future_and_mixed_idx,t-1]
+                    1 
+                    ϵ[:,t]]
+
+        state[:,t] .=  𝐒₁ * aug_state# + solution[3] * ℒ.kron(aug_state, aug_state) / 2 
+    end
+
+    observables_index = sort(indexin(observables, m.timings.var))
+
+    state_deviations = data - state[observables_index,:]
+    
+    Turing.@addlogprob! sum([Turing.logpdf(Turing.MvNormal(Ω * ℒ.I(size(data,1))), state_deviations[:,t]) for t in 1:size(data, 2)])
+end
+
+function draw_shocks(m)
+    x0 ~ Turing.filldist(Turing.Normal(), m.timings.nPast_not_future_and_mixed) # Initial conditions 
+    return x0
+end
