df4662bd5b6fac78490d5015441ca0959b94e9a2
thorek1 <thorek1@users.noreply.github.com>
2023-09-08 19:09:51 +0200

    higer order autocorr in get_stats

--- src/get_functions.jl
+++ src/get_functions.jl
@@ -2379,20 +2379,18 @@ function get_statistics(𝓂,
     if algorithm == :pruned_third_order
 
         if !(autocorrelation == Symbol[])
-            autocorrelation = Symbol[]
-        end
 
-        if !(standard_deviation == Symbol[]) || !(variance == Symbol[]) || !(covariance == Symbol[])
+            covar_dcmp, state_μ, Σᶻ₃, Eᴸᶻ, autocorr_tmp, Σᶻ₂, Δμˢ₂, Σᶻ₁, μˢ₃δμˢ₁, ss_s, s_to_s₁, ŝ_to_ŝ₃, ŝ_to_y₃, ê_to_ŝ₃, ê_to_y₃, SS_and_pars = calculate_third_order_moments(all_parameters, :full_covar, 𝓂, verbose = verbose, autocorrelation = true)
+
+        elseif !(standard_deviation == Symbol[]) || !(variance == Symbol[]) || !(covariance == Symbol[])
+
             covar_dcmp, state_μ, SS_and_pars = calculate_third_order_moments(all_parameters, union(variance,covariance,standard_deviation), 𝓂, verbose = verbose)
+
         end
 
     elseif algorithm == :pruned_second_order
 
-        if !(autocorrelation == Symbol[])
-            autocorrelation = Symbol[]
-        end
-
-        if !(standard_deviation == Symbol[]) || !(variance == Symbol[]) || !(covariance == Symbol[])
+        if !(standard_deviation == Symbol[]) || !(variance == Symbol[]) || !(covariance == Symbol[]) || !(autocorrelation == Symbol[])
             covar_dcmp, Σᶻ₂, state_μ, Δμˢ₂, autocorr_tmp, ŝ_to_ŝ₂, ŝ_to_y₂, Σʸ₁, Σᶻ₁, SS_and_pars, 𝐒₁, ∇₁, 𝐒₂, ∇₂ = calculate_second_order_moments(all_parameters, 𝓂, verbose = verbose)
         else
             state_μ, Δμˢ₂, Σʸ₁, Σᶻ₁, SS_and_pars, 𝐒₁, ∇₁, 𝐒₂, ∇₂ = calculate_second_order_moments(all_parameters, 𝓂, verbose = verbose, covariance = false)
@@ -2410,9 +2408,50 @@ function get_statistics(𝓂,
             st_dev = sqrt.(varrs)
         end
     elseif !(autocorrelation == Symbol[])
-        A = @views sol[:,1:𝓂.timings.nPast_not_future_and_mixed] * ℒ.diagm(ones(𝓂.timings.nVars))[𝓂.timings.past_not_future_and_mixed_idx,:]
+        if algorithm == :pruned_third_order
+            autocorr = zeros(size(covar_dcmp,1),length(autocorrelation_periods))
 
-        autocorr = reduce(hcat,[ℒ.diag(A ^ i * covar_dcmp ./ ℒ.diag(covar_dcmp)) for i in autocorrelation_periods])
+            nˢ = size(s_to_s₁,1)
+            nᵉ = 𝓂.timings.nExo
+
+            s_to_s₁ⁱ = zero(s_to_s₁)
+            s_to_s₁ⁱ += ℒ.diagm(ones(nˢ))
+
+            ŝ_to_ŝ₃ⁱ = zero(ŝ_to_ŝ₃)
+            ŝ_to_ŝ₃ⁱ += ℒ.diagm(ones(size(Σᶻ₃,1)))
+
+            Σᶻ₃ⁱ = Σᶻ₃
+
+            for i in autocorrelation_periods
+                Σᶻ₃ⁱ .= ŝ_to_ŝ₃ * Σᶻ₃ⁱ + ê_to_ŝ₃ * Eᴸᶻ
+                s_to_s₁ⁱ *= s_to_s₁
+
+                Eᴸᶻ = [ spzeros(nᵉ + nᵉ^2 + 2*nᵉ*nˢ + nᵉ*nˢ^2, 3*nˢ + 2*nˢ^2 +nˢ^3)
+                ℒ.kron(s_to_s₁ⁱ * Σᶻ₁,vec(ℒ.I(nᵉ)))   zeros(nˢ*nᵉ^2, nˢ + nˢ^2)  ℒ.kron(s_to_s₁ⁱ * μˢ₃δμˢ₁',vec(ℒ.I(nᵉ)))    ℒ.kron(s_to_s₁ⁱ * reshape(ss_s * vec(Σᶻ₂[nˢ + 1:2*nˢ,2 * nˢ + 1 : end] + Δμˢ₂ * vec(Σᶻ₁)'), nˢ, nˢ^2), vec(ℒ.I(nᵉ)))  ℒ.kron(s_to_s₁ⁱ * reshape(Σᶻ₂[2 * nˢ + 1 : end, 2 * nˢ + 1 : end] + vec(Σᶻ₁) * vec(Σᶻ₁)', nˢ, nˢ^3), vec(ℒ.I(nᵉ)))
+                spzeros(nᵉ^3, 3*nˢ + 2*nˢ^2 +nˢ^3)]
+
+                autocorr[:,i] .= ℒ.diag(ŝ_to_y₃ * Σᶻ₃ⁱ * ŝ_to_y₃' + ŝ_to_y₃ * ŝ_to_ŝ₃ⁱ * autocorr_tmp + ê_to_y₃ * Eᴸᶻ * ŝ_to_y₃') ./ ℒ.diag(covar_dcmp)
+                ŝ_to_ŝ₃ⁱ *= ŝ_to_ŝ₃
+            end
+        elseif algorithm == :pruned_second_order
+            ŝ_to_ŝ₂ⁱ = zero(ŝ_to_ŝ₂)
+            ŝ_to_ŝ₂ⁱ += ℒ.diagm(ones(size(ŝ_to_ŝ₂,1)))
+
+            autocorr = zeros(T,size(covar_dcmp,1),length(autocorrelation_periods))
+
+            for i in autocorrelation_periods
+                autocorr[:,i] .= ℒ.diag(ŝ_to_y₂ * ŝ_to_ŝ₂ⁱ * autocorr_tmp) ./ ℒ.diag(covar_dcmp) 
+                ŝ_to_ŝ₂ⁱ *= ŝ_to_ŝ₂
+            end
+        else
+            A = @views sol[:,1:𝓂.timings.nPast_not_future_and_mixed] * ℒ.diagm(ones(𝓂.timings.nVars))[𝓂.timings.past_not_future_and_mixed_idx,:]
+        
+            autocorr = reduce(hcat,[ℒ.diag(A ^ i * covar_dcmp ./ ℒ.diag(covar_dcmp)) for i in autocorrelation_periods])
+        end
+
+        if !(standard_deviation == Symbol[])
+            st_dev = sqrt.(abs.(convert(Vector{Real},ℒ.diag(covar_dcmp))))
+        end
     else
         if !(standard_deviation == Symbol[])
             st_dev = sqrt.(abs.(convert(Vector{Real},ℒ.diag(covar_dcmp))))
