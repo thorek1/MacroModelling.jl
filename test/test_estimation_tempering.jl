e4b03115b7146460776ae4bde7a61ecf5721eaef
thorek1 <thorek1@users.noreply.github.com>
2024-01-07 11:45:27 +0100

    more draws less chains

--- test/test_estimation_tempering.jl
+++ test/test_estimation_tempering.jl
@@ -38,8 +38,8 @@ FS2000_loglikelihood = FS2000_loglikelihood_function(data, FS2000)
 
 pt = @time Pigeons.pigeons(target = Pigeons.TuringLogPotential(FS2000_loglikelihood_function(data, FS2000)),
             record = [Pigeons.traces; Pigeons.round_trip; Pigeons.record_default()],
-            n_chains = 3,
-            n_rounds = 7,
+            n_chains = 1,
+            n_rounds = 10,
             multithreaded = true)
 
 samps = MCMCChains.Chains(Pigeons.get_sample(pt))
