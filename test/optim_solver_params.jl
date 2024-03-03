4c5c1b0e8c481fa4b2e0ed9d5f0ad5cfce63c357
Thore Kockerols <thorek1@users.noreply.github.com>
2024-03-03 23:04:52 +0100

    Rand ESCH

--- test/optim_solver_params.jl
+++ test/optim_solver_params.jl
@@ -90,7 +90,7 @@ function evaluate_pars_loglikelihood(pars, models)
 end
 
 parameters = [2.9912988764832833, 0.8725, 0.0027, 0.028948770826150612, 8.04, 4.076413176215408, 0.06375413238034794, 0.24284340766769424, 0.5634017580097571, 0.009549630552246828, 0.6342888355132347, 0.5275522227754195, 1.0, 0.06178989216048817, 0.5234277812131813, 0.422, 0.011209254402846185, 0.5047, 0.6020757011698457, 0.7688]
-# parameters = rand(20) .+ 1
+parameters = rand(20) .+ 1
 
 evaluate_pars_loglikelihood(parameters, all_models)
 
@@ -105,18 +105,18 @@ max_minutes = 5 * 60^2 + 30 * 60
 
 # max_minutes = 20
 
-sol_DE = solve(prob, DE(), maxtime = max_minutes); sol_DE.minimum 
+# sol_DE = solve(prob, DE(), maxtime = max_minutes); sol_DE.minimum 
 # sol_SA = solve(prob, SA(), maxtime = max_minutes); sol_SA.minimum 
 # sol_ECA = solve(prob, ECA(), maxtime = max_minutes); sol_ECA.minimum 
 # sol_PSO = solve(prob, PSO(), maxtime = max_minutes); sol_PSO.minimum 
 # sol_WOA = solve(prob, WOA(), maxtime = max_minutes); sol_WOA.minimum 
 # sol_ABC = solve(prob, ABC(), maxtime = max_minutes); sol_ABC.minimum 
 # WOA terminates early
-# sol_ESCH = solve(prob, NLopt.GN_ESCH(), maxtime = max_minutes); sol_ESCH.minimum
+sol_ESCH = solve(prob, NLopt.GN_ESCH(), maxtime = max_minutes); sol_ESCH.minimum
 # sol_CRS = solve(prob, NLopt.GN_CRS2_LM(), maxtime = max_minutes); sol_CRS.minimum # gets nowhere
 # sol_DIRECT = solve(prob, NLopt.GN_DIRECT(), maxtime = max_minutes); sol_DIRECT.minimum
 
-pars = deepcopy(sol_DE.u)
+pars = deepcopy(sol_ESCH.u)
 
 # transform = 0
 # pars = [0.0005091362638940568, 2.8685509141200138e-5, 5.853782207686227e-5, 0.00013196080350191824, 0.0002770476425730156, 0.0009519716244767909, 0.0001214119776051054, 0.00030908501576441285, 0.00015604633597157022, 0.00022641413680158165, 0.00019937397397335106, 0.00024166807372048577, 0.0006155904822177251, 6.834833988676723e-5, 0.0005660185689597568, 0.00023832991638765894, 1.3844380657673424e-5, 6.031788146343705e-5, 0.0002638201993322502, -0.0012275435299784476]
