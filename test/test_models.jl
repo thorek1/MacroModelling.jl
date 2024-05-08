f8a888bc5d94e2078362833253c493acf4b87a2e
Thore Kockerols <thorek1@users.noreply.github.com>
2024-05-08 15:12:26 +0100

    Update test_models.jl fix caldara again

--- test/test_models.jl
+++ test/test_models.jl
@@ -505,9 +505,9 @@ get_loglikelihood(model, simulated_data(observables, :, :simulate), model.parame
 
 back_grad = Zygote.gradient(x-> get_loglikelihood(model, simulated_data(observables, :, :simulate), x), model.parameter_values)
 
-fin_grad = FiniteDifferences.grad(FiniteDifferences.central_fdm(3,1),x-> get_loglikelihood(model, simulated_data(observables, :, :simulate), x), model.parameter_values)
+fin_grad = FiniteDifferences.grad(FiniteDifferences.central_fdm(3,1, max_range = 1e-5),x-> get_loglikelihood(model, simulated_data(observables, :, :simulate), x), model.parameter_values)
 
-@test isapprox(back_grad[1], fin_grad[1], rtol = 1e-4)
+@test isapprox(back_grad[1], fin_grad[1], rtol = 1e-3)
 
 write_to_dynare_file(Caldara_et_al_2012)
 translate_dynare_file("Caldara_et_al_2012.mod")
