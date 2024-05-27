07036620a0f765c7437a51fa63b14f19ccf9c75c
thorek1 <thorek1@users.noreply.github.com>
2024-05-27 12:46:05 +0200

    tested different ways of doing derivatives. no speedup, also MT not useful for derivatives

--- src/MacroModelling.jl
+++ src/MacroModelling.jl
@@ -4992,18 +4992,19 @@ function write_functions_mapping!(𝓂::ℳ, max_perturbation_order::Int; max_ex
     column2 = Int[]
     column3 = Int[]
 
-    Polyester.@batch for rc1 in 0:length(vars_X) * length(eqs_sub) - 1
+    # Polyester.@batch for rc1 in 0:length(vars_X) * length(eqs_sub) - 1
     # for rc1 in 0:length(vars_X) * length(eqs_sub) - 1
-    # for (c1, var1) in enumerate(vars_X)
-        # for (r, eq) in enumerate(eqs_sub)
-        r, c1 = divrem(rc1, length(vars_X)) .+ 1
-        var1 = vars_X[c1]
+    for (c1, var1) in enumerate(vars_X)
+        for (r, eq) in enumerate(eqs_sub)
+        # r, c1 = divrem(rc1, length(vars_X)) .+ 1
+        # var1 = vars_X[c1]
+        # eq = eqs_sub[r]
             if Symbol(var1) ∈ Symbol.(Symbolics.get_variables(eq))
                 deriv_first = Symbolics.derivative(eq, var1)
                 
                 push!(first_order, deriv_first)
-                # push!(row1, r)
-                push!(row1, r...)
+                push!(row1, r)
+                # push!(row1, r...)
                 push!(column1, c1)
                 if max_perturbation_order >= 2 
                     for (c2, var2) in enumerate(vars_X)
@@ -5028,7 +5029,7 @@ function write_functions_mapping!(𝓂::ℳ, max_perturbation_order::Int; max_ex
                     end
                 end
             end
-        # end
+        end
     end
     
     if max_perturbation_order >= 1
