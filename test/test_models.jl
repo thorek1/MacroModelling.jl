781494ef621885a278a370958fcde8d3e5f8fc8f
thorek1 <thorek1@users.noreply.github.com>
2024-07-06 12:28:54 +0200

    add comment

--- test/test_models.jl
+++ test/test_models.jl
@@ -296,7 +296,8 @@ if !test_higher_order
     get_loglikelihood(model, simulated_data(observables, :, :simulate), model.parameter_values)
 
     back_grad = Zygote.gradient(x-> get_loglikelihood(model, simulated_data(observables, :, :simulate), x), model.parameter_values)
-
+    
+    # use forward_cdm so that parameter values stay positive. they would return NaN otherwise
     fin_grad = FiniteDifferences.grad(FiniteDifferences.forward_fdm(3,1),x-> get_loglikelihood(model, simulated_data(observables, :, :simulate), x), model.parameter_values)
     
     @test isapprox(back_grad[1], fin_grad[1], rtol = 1e-4)
