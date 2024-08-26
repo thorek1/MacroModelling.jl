9e1b00221a4075d5987e036a89eab629614bbb31
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2024-08-26 14:30:43 +0000

    try to fix nan failure in test models

--- test/test_models.jl
+++ test/test_models.jl
@@ -295,13 +295,19 @@ if !test_higher_order
 
     get_loglikelihood(model, simulated_data(observables, :, :simulate), model.parameter_values)
     
-    SS(model, parameters = [:alpha => 0.1, :trend_inflation => 1.5, :var_rho => 0.01]) # avoid the NaN error for finitediff in tests
+    # SS(model, parameters = [:alpha => 0.1, :trend_inflation => 1.5, :var_rho => 0.01]) # avoid the NaN error for finitediff in tests
 
     back_grad = Zygote.gradient(x-> get_loglikelihood(model, simulated_data(observables, :, :simulate), x), model.parameter_values)
     
     # use forward_cdm so that parameter values stay positive. they would return NaN otherwise
     fin_grad = FiniteDifferences.grad(FiniteDifferences.central_fdm(4,1, max_range = 1e-4),x-> get_loglikelihood(model, simulated_data(observables, :, :simulate), x), model.parameter_values)
     
+    i = 1
+    while !isfinite(ℒ.norm(fin_grad)) && i < 100
+        fin_grad = FiniteDifferences.grad(FiniteDifferences.forward_fdm(4,1, max_range = 1e-5),x-> get_loglikelihood(model, simulated_data(observables, :, :simulate), x), model.parameter_values)
+        i += 1
+    end
+
     @test isapprox(back_grad[1], fin_grad[1], rtol = 1e-4)
 
     write_to_dynare_file(Ascari_Sbordone_2014)
