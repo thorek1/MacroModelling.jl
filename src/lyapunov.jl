431fbf2986466d24f2ab35d7a3f9567e36d2c32e
thorek1 <thorek1@users.noreply.github.com>
2024-09-09 20:54:34 +0200

    correct todo

--- src/lyapunov.jl
+++ src/lyapunov.jl
@@ -155,7 +155,7 @@ function solve_lyapunov_equation(   A::Union{ℒ.Adjoint{Float64,Matrix{Float64}
     iters = max_iter
 
     for i in 1:max_iter
-        𝐂¹ = 𝐀 * 𝐂 * 𝐀' + 𝐂 % TODO check whether this is sparse in some cases
+        𝐂¹ = 𝐀 * 𝐂 * 𝐀' + 𝐂 # TODO check whether this is sparse in some cases
 
         mul!(𝐀², 𝐀, 𝐀)
         copyto!(𝐀, 𝐀²)
