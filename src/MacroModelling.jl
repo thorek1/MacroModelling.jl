35888dc04bcb2865bcd5508b8193d4eb02647812
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2024-11-08 23:16:21 +0000

    comments on other things to try

--- src/MacroModelling.jl
+++ src/MacroModelling.jl
@@ -3138,7 +3138,7 @@ function solve_steady_state!(𝓂::ℳ, symbolic_SS, Symbolics::symbolics; verbo
                     solved_scale = 0
                     # range_length = [ 1, 2, 4, 8,16,32,64,128,1024]
                     scale = 1.0
-
+                    # TODO: try have this run for 1000 and and use closest_solution based on the previous result and not on the cache. that way you dont crowd out good and diverse solutions in the cache and make sure he finds the other SS. rely on previous commit for way of implementing closest_solution
                     while range_iters < 500 && !(solution_error < 1e-12 && solved_scale == 1)
                         range_iters += 1
                         # println(range_iters)
