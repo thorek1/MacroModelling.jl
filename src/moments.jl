9eb60f198fe381528a3d366cf62ee8b1c74852f8
thorek1 <thorek1@users.noreply.github.com>
2025-01-03 17:00:13 +0100

    fix autocorr third order

--- src/moments.jl
+++ src/moments.jl
@@ -635,19 +635,13 @@ function calculate_third_order_moments(parameters::Vector{T},
                 spzeros(nᵉ^3, 3*nˢ + 2*nˢ^2 +nˢ^3)]
 
                 for obs in variance_observable
-                    #autocorr_tmp = (ℒ.diag(ŝ_to_y₃ * Σᶻ₃ⁱ * ŝ_to_y₃' + ŝ_to_y₃ * ŝ_to_ŝ₃ⁱ * autocorr_tmp + ê_to_y₃ * Eᴸᶻ * ŝ_to_y₃') ./ max.(ℒ.diag(Σʸ₃tmp), eps(Float64)))[indexin([obs], variance_observable)]
+					autocorr[indexin([obs], 𝓂.timings.var), i] .= ℒ.diag(ŝ_to_y₃ * Σᶻ₃ⁱ * ŝ_to_y₃' + ŝ_to_y₃ * ŝ_to_ŝ₃ⁱ * autocorr_tmp + ê_to_y₃ * Eᴸᶻ * ŝ_to_y₃')[indexin([obs], variance_observable)] ./ max.(ℒ.diag(Σʸ₃tmp), eps(Float64))[indexin([obs], variance_observable)]
 
-                    #autocorr_tmp[ℒ.diag(Σʸ₃tmp)[indexin([obs], variance_observable)] .< opts.tol.lyapunov_acceptance_tol] .= 0
-
-                    #autocorr[indexin([obs], 𝓂.timings.var), i] .= autocorr_tmp
-										autocorr[indexin([obs], 𝓂.timings.var), i] .= (ℒ.diag(ŝ_to_y₃ * Σᶻ₃ⁱ * ŝ_to_y₃' + ŝ_to_y₃ * ŝ_to_ŝ₃ⁱ * autocorr_tmp + ê_to_y₃ * Eᴸᶻ * ŝ_to_y₃') ./ max.(ℒ.diag(Σʸ₃tmp), eps(Float64)))[indexin([obs], variance_observable)]
-										
-										autocorr[ℒ.diag(Σʸ₃tmp) .< opts.tol.lyapunov_acceptance_tol,:] .= 0
+                    autocorr[indexin([obs], 𝓂.timings.var), i][ℒ.diag(Σʸ₃tmp)[indexin([obs], variance_observable)] .< opts.tol.lyapunov_acceptance_tol] .= 0
                 end
 
                 ŝ_to_ŝ₃ⁱ *= ŝ_to_ŝ₃
             end
-
         end
     end
 
