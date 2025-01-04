e2069ff9902b818f2bd6aba3a493dcf72586ffba
thorek1 <thorek1@users.noreply.github.com>
2025-01-04 22:26:11 +0100

    use forward instead of central fdm for findiff

--- test/functionality_tests.jl
+++ test/functionality_tests.jl
@@ -746,7 +746,7 @@ function functionality_test(m; algorithm = :first_order, plots = true)
                     
                     deriv_for = ForwardDiff.jacobian(x->get_irf(m, x, initial_state = initial_state)[:,1,1], parameter_values)
 
-                    deriv_fin = FiniteDifferences.jacobian(FiniteDifferences.central_fdm(4,1), x->get_irf(m, x, initial_state = initial_state)[:,1,1], parameter_values)
+                    deriv_fin = FiniteDifferences.jacobian(FiniteDifferences.forward_fdm(4,1), x->get_irf(m, x, initial_state = initial_state)[:,1,1], parameter_values)
 
                     @test isapprox(deriv_for, deriv_fin[1], rtol = 1e-6)
 
@@ -815,7 +815,7 @@ function functionality_test(m; algorithm = :first_order, plots = true)
 
             deriv_sol_fin = []
             for i in 1:length(sol)-2
-                push!(deriv_sol_fin, FiniteDifferences.jacobian(FiniteDifferences.central_fdm(3,1),
+                push!(deriv_sol_fin, FiniteDifferences.jacobian(FiniteDifferences.forward_fdm(3,1),
                                                     x->get_solution(m, x, algorithm = algorithm)[i], parameter_values)[1])
             end
 
@@ -1005,7 +1005,7 @@ function functionality_test(m; algorithm = :first_order, plots = true)
         deriv1 = ForwardDiff.jacobian(x->get_statistics(m, x, algorithm = algorithm, 
                                                         non_stochastic_steady_state = m.var)[:non_stochastic_steady_state], old_params)
 
-        deriv1_fin = FiniteDifferences.jacobian(FiniteDifferences.central_fdm(3,1),
+        deriv1_fin = FiniteDifferences.jacobian(FiniteDifferences.forward_fdm(3,1),
                                                 x->get_statistics(m, x, algorithm = algorithm, 
                                                         non_stochastic_steady_state = m.var)[:non_stochastic_steady_state], old_params)
 
@@ -1036,7 +1036,7 @@ function functionality_test(m; algorithm = :first_order, plots = true)
                                                             mean = m.var)[:mean], old_params)
             
 
-            deriv2_fin = FiniteDifferences.jacobian(FiniteDifferences.central_fdm(4,1,max_range = 1e-4),
+            deriv2_fin = FiniteDifferences.jacobian(FiniteDifferences.forward_fdm(4,1,max_range = 1e-4),
                                                             x->get_statistics(m, x, algorithm = algorithm, 
                                                             mean = m.var)[:mean], old_params)
                           
@@ -1065,7 +1065,7 @@ function functionality_test(m; algorithm = :first_order, plots = true)
             deriv3 = ForwardDiff.jacobian(x->get_statistics(m, x, algorithm = algorithm, 
                                                             standard_deviation = m.var)[:standard_deviation], old_params)
             
-            deriv3_fin = FiniteDifferences.jacobian(FiniteDifferences.central_fdm(4,1,max_range = 1e-4),
+            deriv3_fin = FiniteDifferences.jacobian(FiniteDifferences.forward_fdm(4,1,max_range = 1e-4),
                                                             x->get_statistics(m, x, algorithm = algorithm, 
                                                             standard_deviation = m.var)[:standard_deviation], old_params)
                         
@@ -1093,7 +1093,7 @@ function functionality_test(m; algorithm = :first_order, plots = true)
             deriv4 = ForwardDiff.jacobian(x->get_statistics(m, x, algorithm = algorithm, 
                                                             variance = m.var)[:variance], old_params)
 
-            deriv4_fin = FiniteDifferences.jacobian(FiniteDifferences.central_fdm(4,1,max_range = 1e-4),
+            deriv4_fin = FiniteDifferences.jacobian(FiniteDifferences.forward_fdm(4,1,max_range = 1e-4),
                                                             x->get_statistics(m, x, algorithm = algorithm, 
                                                             variance = m.var)[:variance], old_params)
                     
@@ -1123,7 +1123,7 @@ function functionality_test(m; algorithm = :first_order, plots = true)
                                                             sylvester_acceptance_tol = 1e-14),
                                                             covariance = m.var)[:covariance], old_params)
 
-            deriv5_fin = FiniteDifferences.jacobian(FiniteDifferences.central_fdm(4,1,max_range = 1e-4),
+            deriv5_fin = FiniteDifferences.jacobian(FiniteDifferences.forward_fdm(4,1,max_range = 1e-4),
                                                             x->get_statistics(m, x, algorithm = algorithm, 
                                                             tol = MacroModelling.Tolerances(NSSS_xtol = 1e-14, lyapunov_acceptance_tol = 1e-14, 
                                                             sylvester_acceptance_tol = 1e-14),
