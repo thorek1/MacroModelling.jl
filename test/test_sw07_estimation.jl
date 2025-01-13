4851748781de8f036b7c5c58e4be7a221b6cfd48
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2025-01-13 10:33:05 +0000

    no inits for sw07 estimation

--- test/test_sw07_estimation.jl
+++ test/test_sw07_estimation.jl
@@ -94,9 +94,9 @@ SS(Smets_Wouters_2007_linear, parameters = [:crhoms => 0.01, :crhopinf => 0.01, 
 SW07_loglikelihood = SW07_loglikelihood_function(data, Smets_Wouters_2007_linear, observables, fixed_parameters, :kalman)
 # inversion filter delivers similar results
 
-par_names = [:z_ea, :z_eb, :z_eg, :z_eqs, :z_em, :z_epinf, :z_ew, :crhoa, :crhob, :crhog, :crhoqs, :crhoms, :crhopinf, :crhow, :cmap, :cmaw, :csadjcost, :csigma, :chabb, :cprobw, :csigl, :cprobp, :cindw, :cindp, :czcap, :cfc, :crpi, :crr, :cry, :crdy, :constepinf, :constebeta, :constelab, :ctrend, :cgy, :calfa]
+# par_names = [:z_ea, :z_eb, :z_eg, :z_eqs, :z_em, :z_epinf, :z_ew, :crhoa, :crhob, :crhog, :crhoqs, :crhoms, :crhopinf, :crhow, :cmap, :cmaw, :csadjcost, :csigma, :chabb, :cprobw, :csigl, :cprobp, :cindw, :cindp, :czcap, :cfc, :crpi, :crr, :cry, :crdy, :constepinf, :constebeta, :constelab, :ctrend, :cgy, :calfa]
 
-inits = [Dict(get_parameters(Smets_Wouters_2007_linear, values = true))[string(i)] for i in par_names]
+# inits = [Dict(get_parameters(Smets_Wouters_2007_linear, values = true))[string(i)] for i in par_names]
 
 # modeSW2007 = Turing.maximum_a_posteriori(SW07_loglikelihood, 
 #                                         Optim.SimulatedAnnealing())#,
@@ -111,10 +111,12 @@ inits = [Dict(get_parameters(Smets_Wouters_2007_linear, values = true))[string(i
 
 # println("Mode variable values (linear): $(modeSW2007.values); Mode loglikelihood: $(modeSW2007.lp)")
 
+# LLH = Turing.logjoint(SW07_loglikelihood, (all_params = inits,))
+
 n_samples = 1000
 
 samps = @time Turing.sample(SW07_loglikelihood, NUTS(adtype = AutoZygote()), n_samples, 
-                            initial_params = inits,
+                            # initial_params = inits,
                             progress = true)
 
 println(samps)
@@ -151,7 +153,7 @@ SW07_loglikelihood = SW07_loglikelihood_function(data, Smets_Wouters_2007, obser
 n_samples = 1000
 
 samps = @time Turing.sample(SW07_loglikelihood, NUTS(adtype = AutoZygote()), n_samples, 
-                            initial_params = inits,
+                            # initial_params = inits,
                             progress = true)
 
 println(samps)
