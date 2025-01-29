937d1af98032d378c0103a7eb0dcf55419a30b3a
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2025-01-29 14:33:36 +0000

    put fs2000 3rd order first in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -319,30 +319,30 @@ if test_set == "higher_order_1"
     plots = true
     # test_higher_order = true
 
-    @testset verbose = true "FS2000 second order" begin
-        include("../models/FS2000.jl")
-        functionality_test(FS2000, algorithm = :second_order, plots = plots)
-    end
-    # FS2000 = nothing
-    GC.gc()
-
-    @testset verbose = true "FS2000 pruned second order" begin
-        include("../models/FS2000.jl")
-        functionality_test(FS2000, algorithm = :pruned_second_order, plots = plots)
-    end
-    # FS2000 = nothing
-    GC.gc()
-
     @testset verbose = true "FS2000 third order" begin
         include("../models/FS2000.jl")
         functionality_test(FS2000, algorithm = :third_order, plots = plots)
     end
-    # FS2000 = nothing
+    FS2000 = nothing
     GC.gc()
 
     @testset verbose = true "FS2000 pruned third order" begin
         include("../models/FS2000.jl")
         functionality_test(FS2000, algorithm = :pruned_third_order, plots = plots)
+    end
+    FS2000 = nothing
+    GC.gc()
+
+    @testset verbose = true "FS2000 second order" begin
+        include("../models/FS2000.jl")
+        functionality_test(FS2000, algorithm = :second_order, plots = plots)
+    end
+    FS2000 = nothing
+    GC.gc()
+
+    @testset verbose = true "FS2000 pruned second order" begin
+        include("../models/FS2000.jl")
+        functionality_test(FS2000, algorithm = :pruned_second_order, plots = plots)
     end
     FS2000 = nothing
     GC.gc()
