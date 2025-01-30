1314299471e6073ddb2457aee5ca8344e735b1b9
thorek1 <thorek1@users.noreply.github.com>
2025-01-30 16:51:58 +0100

    fix typo

--- src/MacroModelling.jl
+++ src/MacroModelling.jl
@@ -640,7 +640,7 @@ end
 
 
 function clear_solution_caches!(𝓂::ℳ, algorithm::Symbol)
-    push!(m.solution.outdated_algorithms, [:first_order, :pruned_second_order, :second_order, :pruned_third_order, :third_order])
+    push!(𝓂.solution.outdated_algorithms, [:first_order, :pruned_second_order, :second_order, :pruned_third_order, :third_order])
 
     while length(𝓂.NSSS_solver_cache) > 1
         pop!(𝓂.NSSS_solver_cache)
