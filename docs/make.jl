3355b12f9c0fc485b0504f958c231898d733a01c
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2026-01-07 16:07:08 +0000

    move docs

--- docs/make.jl
+++ docs/make.jl
@@ -41,10 +41,10 @@ makedocs(
             "Variance Decomposition" => "plot_conditional_variance_decomposition.md",
             "Model Estimates" => "plot_model_estimates.md",
         ],
+        "Steady State" => "steady_state.md",
         "How-to guides" => [
             "Programmatic model writing using for-loops" => "how-to/loops.md",
             "Occasionally binding constraints" => "how-to/obc.md",
-            "Steady State" => "how-to/steady_state.md",
             # "how_to.md"
             ],
         # "Model syntax" => "dsl.md",
