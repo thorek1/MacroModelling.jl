8966a88478698fa12e2a8d7f3ff1d44e69da0d5a
thorek1 <thorek1@users.noreply.github.com>
2026-01-11 23:23:26 +0100

    Add script to test get_loglikelihood with pruned third order

---
+++ scripts/test_get_loglikelihood_pruned_third.jl
@@ -0,0 +1,14 @@
+using MacroModelling
+using Random
+
+# minimal script to test get_loglikelihood with pruned third order
+include(joinpath(@__DIR__, "..", "models", "RBC_baseline.jl"))
+
+Random.seed!(123)
+
+sim = simulate(RBC_baseline)
+data = sim([:y], :, :simulate)
+
+llh = get_loglikelihood(RBC_baseline, data, RBC_baseline.parameter_values, algorithm = :pruned_third_order, verbose = true)
+
+println("LLH (pruned_third_order): ", llh)
