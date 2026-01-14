be213f2fed87ca20d0deddd3df58b180269aceb3
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2026-01-14 11:44:24 +0000

    del redundant wrapper

--- src/algorithms/quadratic_matrix_equation.jl
+++ src/algorithms/quadratic_matrix_equation.jl
@@ -82,24 +82,6 @@ end
 function solve_quadratic_matrix_equation(A::AbstractMatrix{R}, 
                                         B::AbstractMatrix{R}, 
                                         C::AbstractMatrix{R}, 
-                                        T::timings; 
-                                        initial_guess::AbstractMatrix{R} = zeros(0,0),
-                                        quadratic_matrix_equation_algorithm::Symbol = :schur,
-                                        tol::AbstractFloat = 1e-14,
-                                        acceptance_tol::AbstractFloat = 1e-8,
-                                        verbose::Bool = false) where R <: Real
-    caches = set_timings!(Caches(), T)
-    return solve_quadratic_matrix_equation(A, B, C, caches;
-                                            initial_guess = initial_guess,
-                                            quadratic_matrix_equation_algorithm = quadratic_matrix_equation_algorithm,
-                                            tol = tol,
-                                            acceptance_tol = acceptance_tol,
-                                            verbose = verbose)
-end
-
-function solve_quadratic_matrix_equation(A::AbstractMatrix{R}, 
-                                        B::AbstractMatrix{R}, 
-                                        C::AbstractMatrix{R}, 
                                         ::Val{:schur}, 
                                         T::timings; 
                                         initial_guess::AbstractMatrix{R} = zeros(0,0),
