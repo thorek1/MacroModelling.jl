7bcc4c693b65183393eb967afc7d644f249b5c0f
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2026-02-17 21:44:35 +0000

    update bench script

--- benchmark/sw07_random_parameter_ss_benchmark.jl
+++ benchmark/sw07_random_parameter_ss_benchmark.jl
@@ -8,32 +8,56 @@ include(joinpath(@__DIR__, "..", "models", "Smets_Wouters_2007.jl"))
 
 model = Smets_Wouters_2007
 
+init_pars = deepcopy(model.parameter_values)
 # Warm-up to ensure NSSS solver infrastructure and initial cache are available.
 get_steady_state(model, derivatives = false)
 
 trial = @benchmark begin
-    get_steady_state($model, parameters = $model.parameter_values, derivatives = false)
-end setup = clear_solution_caches!($model,:first_order)
+    # get_steady_state($model, parameters = $init_pars .+ .001, derivatives = false)
+    get_solution($model,  $init_pars .+ .001)
+end setup = while length(model.caches.solver_cache) > 2
+    pop!(model.caches.solver_cache)
+end
 
-@profview_allocs for i in 1:1000 
-    clear_solution_caches!(model,:first_order)
-    get_steady_state(model, parameters = model.parameter_values, derivatives = false)
+
+@profview_allocs for i in 1:10000 
+    while length(model.caches.solver_cache) > 2
+        pop!(model.caches.solver_cache)
+    end
+
+    get_solution(model, init_pars .+ .001)
+    # get_steady_state(model, parameters = init_pars .+ .001, derivatives = false)
 end
 
 
 @profview for i in 1:100000
-    clear_solution_caches!(model,:first_order)
-    get_steady_state(model, parameters = model.parameter_values, derivatives = false)
+    while length(model.caches.solver_cache) > 2
+        pop!(model.caches.solver_cache)
+    end
+
+    get_solution(model, init_pars .+ .001)
+    # get_steady_state(model, parameters = init_pars .+ .001, derivatives = false)
 end
 
-import MacroModelling: update_post_complete_parameters
-model.constants.post_complete_parameters = update_post_complete_parameters(
-            model.constants.post_complete_parameters;
-            nsss_fastest_solver_parameter_idx = 13,
-        );
+# import MacroModelling: update_post_complete_parameters
+# model.constants.post_complete_parameters = update_post_complete_parameters(
+#             model.constants.post_complete_parameters;
+#             nsss_fastest_solver_parameter_idx = 13,
+#         );
 
-clear_solution_caches!(model,:first_order)
-get_steady_state(model, parameters = model.parameter_values, derivatives = false, verbose = true)
+while length(model.caches.solver_cache) > 1
+    pop!(model.caches.solver_cache)
+end
+
+get_steady_state(model, parameters = init_pars, derivatives = false, verbose = true)
+
+
+while length(model.caches.solver_cache) > 2
+    pop!(model.caches.solver_cache)
+end
+
+get_steady_state(model, parameters = init_pars .+ .001, derivatives = false, verbose = true)
+
 
 model.caches.solver_cache
 
