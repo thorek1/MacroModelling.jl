7f5f14682430a949f033eec828fe3185e846b5be
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2026-05-02 21:14:42 +0000

    DD errors

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@
 test_set = ENV["TEST_SET"]
 using Preferences: set_preferences!
 set_preferences!("MacroModelling", "dispatch_doctor_mode" => test_set in ["estimate_sw07", "estimate_sw07_nested_sampling", "estimation", "1st_order_inversion_estimation", "pruned_2nd_order_estimation", "2nd_order_estimation", "pruned_3rd_order_estimation", "3rd_order_estimation", "estimation_pigeons", "1st_order_inversion_estimation_pigeons", "2nd_order_estimation_pigeons", "pruned_2nd_order_estimation_pigeons", "3rd_order_estimation_pigeons", "pruned_3rd_order_estimation_pigeons", "gali_pruned_2nd_order_estimation", "rrule_robustness", "inversion_filter_gradients"
-] ? "disable" : "warning")
+] ? "disable" : "error")
 set_preferences!("MacroModelling", "dispatch_doctor_union_limit" => 4)
 
 println("Running test set: $test_set")
