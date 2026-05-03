480394c35c853fdf5bd27be5b82a5a063a3e3ada
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2026-05-03 11:15:49 +0000

    rm dead code

--- test/test_helpers.jl
+++ test/test_helpers.jl
@@ -17,16 +17,3 @@ function quarterly_dates(start_date::Date, len::Int)
     end
     return dates
 end
-
-
-
-# function pigeons_flexichain(samples::AbstractArray{<:Real,3}, names::AbstractVector)
-#     n_iters, _, n_chains = size(samples)
-#     symbol_names = Symbol.(collect(names))
-#     data = OrderedDict{FlexiChains.ParameterOrExtra{Symbol}, Matrix{eltype(samples)}}()
-#     for (column, name) in pairs(symbol_names)
-#         key = name == :log_density ? Extra(name) : Parameter(name)
-#         data[key] = Matrix(@view samples[:, column, :])
-#     end
-#     return FlexiChain{Symbol}(n_iters, n_chains, data)
-# end
