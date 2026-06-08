54c41896ded0630a0015576d776e6fdcf526aa74
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2026-06-08 21:31:25 +0000

    Add filter-free shock tests to functionality_tests and remove filter_free_plotting from runtests

--- test/functionality_tests.jl
+++ test/functionality_tests.jl
@@ -516,6 +516,291 @@ function functionality_test(m, m2; algorithm = :first_order, plots = true)
                                             forecast_periods = forecast_periods)
                 end
             end
+
+            # Test filter-free positional shocks
+            if algorithm in [:first_order, :second_order, :pruned_second_order, :third_order, :pruned_third_order]
+                nExo = length(m.constants.post_model_macro.exo)
+                nT = size(data, 2)
+                zero_shocks = zeros(nExo, nT)
+
+                obs_axis = collect(axiskeys(data_in_levels, 1))
+                n_obs = length(obs_axis)
+                scalar_std = 0.01
+                vector_std = fill(scalar_std, n_obs)
+                matrix_std = fill(scalar_std, n_obs, nT)
+                matrix_std[:, 2:2:end] .= 0.02
+
+                for presample_periods in [0, 3]
+                    clear_solution_caches!(m, algorithm)
+
+                    plot_model_estimates(m, data, zero_shocks,
+                                            algorithm = algorithm,
+                                            data_in_levels = false,
+                                            presample_periods = presample_periods)
+                end
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, zero_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data, zero_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = false,
+                                        forecast_periods = 6)
+
+                for presample_periods in [0, 3]
+                    clear_solution_caches!(m, algorithm)
+
+                    plot_model_estimates!(m, data, zero_shocks,
+                                            algorithm = algorithm,
+                                            data_in_levels = false,
+                                            presample_periods = presample_periods)
+                end
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, zero_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # filter-free + scalar measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data, zero_shocks, scalar_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = false)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data, zero_shocks, scalar_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = false)
+
+                # filter-free + vector measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data, zero_shocks, vector_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = false)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data, zero_shocks, vector_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = false)
+
+                # filter-free + matrix measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data, zero_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = false)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data, zero_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = false)
+
+                # filter-free + measurement_error_std + data_in_levels
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, zero_shocks, scalar_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, zero_shocks, vector_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, zero_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # filter-free + measurement_error_std + forecast_periods
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data, zero_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = false,
+                                        forecast_periods = 6)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data, zero_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = false,
+                                        forecast_periods = 6)
+            end
+
+            # Test Kalman-filtered shocks as positional shocks (first_order only)
+            if algorithm == :first_order
+                obs_axis = collect(axiskeys(data_in_levels, 1))
+                n_obs = length(obs_axis)
+                nT = size(data_in_levels, 2)
+                scalar_std = 0.01
+                vector_std = fill(scalar_std, n_obs)
+                matrix_std = fill(scalar_std, n_obs, nT)
+                matrix_std[:, 2:2:end] .= 0.02
+
+                clear_solution_caches!(m, algorithm)
+
+                smoothed_shocks = Matrix(get_estimated_shocks(m, data_in_levels,
+                                                                algorithm = algorithm,
+                                                                filter = :kalman,
+                                                                smooth = true,
+                                                                data_in_levels = true))
+
+                clear_solution_caches!(m, algorithm)
+
+                filtered_shocks = Matrix(get_estimated_shocks(m, data_in_levels,
+                                                                algorithm = algorithm,
+                                                                filter = :kalman,
+                                                                smooth = false,
+                                                                data_in_levels = true))
+
+                # kalman smoothed + data_in_levels
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, smoothed_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, smoothed_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # kalman smoothed + forecast_periods
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, smoothed_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = true,
+                                        forecast_periods = 6)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, smoothed_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = true,
+                                        forecast_periods = 6)
+
+                # kalman filtered + data_in_levels
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, filtered_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, filtered_shocks,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # kalman smoothed + scalar measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, smoothed_shocks, scalar_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, smoothed_shocks, scalar_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # kalman smoothed + vector measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, smoothed_shocks, vector_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, smoothed_shocks, vector_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # kalman smoothed + matrix measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, smoothed_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, smoothed_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # kalman filtered + scalar measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, filtered_shocks, scalar_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, filtered_shocks, scalar_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # kalman filtered + vector measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, filtered_shocks, vector_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, filtered_shocks, vector_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # kalman filtered + matrix measurement_error_std
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, filtered_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, filtered_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true)
+
+                # kalman smoothed + matrix measurement_error_std + forecast_periods
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates(m, data_in_levels, smoothed_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true,
+                                        forecast_periods = 6)
+
+                clear_solution_caches!(m, algorithm)
+
+                plot_model_estimates!(m, data_in_levels, smoothed_shocks, matrix_std,
+                                        algorithm = algorithm,
+                                        data_in_levels = true,
+                                        forecast_periods = 6)
+            end
         end
 
         @testset "plot_solution" begin
