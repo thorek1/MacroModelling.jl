a5e1bed3fa90b57181c9440f0b98c74884d32a6f
Thore Kockerols <Thore.Kockerols@ecb.europa.eu>
2026-06-15 14:15:33 +0000

    clean up bgg and gk model notation

--- models/Bernanke_Gertler_Gilchrist_1999_nonlinear.jl
+++ models/Bernanke_Gertler_Gilchrist_1999_nonlinear.jl
@@ -1,101 +1,101 @@
 using MacroModelling
 
 @model Bernanke_Gertler_Gilchrist_1999_nonlinear begin
-    # Household Euler equation: intertemporal optimality for deposits.
-    C[0]^(-1) = β * C[1]^(-1) * R[0] / Pi[1]
+    # [B.3] Household Euler equation: intertemporal optimality for deposits.
+    C[0]^(-1) = β * C[1]^(-1) * R[0] / π[1]
 
-    # Household intratemporal condition: consumption-leisure tradeoff.
+    # [B.4] Household intratemporal condition: consumption-leisure tradeoff.
     ζ * C[0] / (1 - H[0]) = W[0]
 
-    # Labor aggregation with entrepreneurial labor share Ω.
-    L[0] = H[0]^(1 - Ω)
+    # [4.6] Labor aggregation with entrepreneurial labor share Ω (Hᵉ normalized to 1).
+    L[0] = H[0]^(1 - Ω) * Hᵉ^Ω
 
-    # Final goods technology: Cobb-Douglas production.
+    # [4.1] Final goods technology: Cobb-Douglas production.
     Y[0] = A[0] * K[-1]^α * L[0]^(1 - α)
 
-    # Household wage from marginal product of household labor.
+    # [4.11] Household labor demand: wage equals marginal product.
     W[0] = (1 - α) * (1 - Ω) * Y[0] / (X[0] * H[0])
 
-    # Entrepreneurial labor income from marginal product of entrepreneurial labor.
-    W_e[0] = (1 - α) * Ω * Y[0] / X[0]
+    # [4.12] Entrepreneurial labor income from marginal product.
+    Wᵉ[0] = (1 - α) * Ω * Y[0] / X[0]
 
-    # Gross return on capital before external finance costs.
-    Rk[0] = (α * Y[0] / (X[0] * K[-1]) + (1 - δ) * Q[0]) / Q[-1]
+    # [4.4] Gross return on capital (Rᵏ = R^k in the paper).
+    Rᵏ[0] = (α * Y[0] / (X[0] * K[-1]) + (1 - δ) * Q[0]) / Q[-1]
 
-    # Capital accumulation with convex investment adjustment costs.
-    K[0] = (1 - δ) * K[-1] + (I[0] / K[-1] - ψ_i / 2 * (I[0] / K[-1] - δ)^2) * K[-1]
+    # [4.2] Capital accumulation with convex investment adjustment costs.
+    K[0] = (1 - δ) * K[-1] + (I[0] / K[-1] - ψⁱ / 2 * (I[0] / K[-1] - δ)^2) * K[-1]
 
-    # Tobin's Q implied by the marginal adjustment cost of investment.
-    Q[0] = 1 / (1 - ψ_i * (I[0] / K[-1] - δ))
+    # [4.3] Tobin's Q implied by the marginal adjustment cost of investment.
+    Q[0] = 1 / (1 - ψⁱ * (I[0] / K[-1] - δ))
 
-    # Aggregate resource constraint including entrepreneurial consumption and monitoring costs.
-    Y[0] = C[0] + Ce[0] + I[0] + Gov[0] + monitoring_cost[0]
+    # [B.8] Aggregate resource constraint including entrepreneurial consumption and monitoring costs.
+    Y[0] = C[0] + Cᵉ[0] + I[0] + Gov[0] + monitoring_cost[0]
 
-    # Monetary policy rule used in the paper's quantitative model.
-    R[0] = (Pi_ss / β)^(1 - ρ_R) * R[-1]^ρ_R * (Pi[0] / Pi_ss)^((1 - ρ_R) * ϕ_pi) * (Y[0] / Y[ss])^((1 - ρ_R) * ϕ_y) * exp(std_R * eps_R[x])
+    # [4.25] Monetary policy rule used in the paper's quantitative model.
+    R[0] = (πˢˢ / β)^(1 - ρʳ) * R[-1]^ρʳ * (π[0] / πˢˢ)^((1 - ρʳ) * ϕ_pi) * (Y[0] / Y[ss])^((1 - ρʳ) * ϕʸ) * exp(σʳ * εʳ[x])
 
-    # Calvo price index.
-    1.0 = θ * Pi[0]^(ϵ_p - 1) + (1 - θ) * Pi_star[0]^(1 - ϵ_p)
+    # [B.12] Calvo price index.
+    1.0 = θ * π[0]^(ϵᵖ - 1) + (1 - θ) * πstar[0]^(1 - ϵᵖ)
 
-    # Optimal reset price from the Calvo price-setting FOC.
-    Pi_star[0] = ϵ_p / (ϵ_p - 1) * price_aux_1[0] / price_aux_2[0]
+    # [B.11] Optimal reset price from the Calvo price-setting FOC.
+    πstar[0] = ϵᵖ / (ϵᵖ - 1) * price_aux_1[0] / price_aux_2[0]
 
-    # Calvo numerator recursion for the optimal reset price.
-    price_aux_1[0] = Y[0] / X[0] / C[0] + β * θ * Pi[1]^ϵ_p * price_aux_1[1]
+    # [B.11a] Calvo numerator recursion.
+    price_aux_1[0] = Y[0] / X[0] / C[0] + β * θ * π[1]^ϵᵖ * price_aux_1[1]
 
-    # Calvo denominator recursion for the optimal reset price.
-    price_aux_2[0] = Y[0] / C[0] + β * θ * Pi[1]^(ϵ_p - 1) * price_aux_2[1]
+    # [B.11b] Calvo denominator recursion.
+    price_aux_2[0] = Y[0] / C[0] + β * θ * π[1]^(ϵᵖ - 1) * price_aux_2[1]
 
-    # Aggregate technology process.
-    log(A[0]) = ρ_A * log(A[-1]) + std_A * eps_A[x]
+    # [4.27] Aggregate technology process.
+    log(A[0]) = ρᵃ * log(A[-1]) + σᵃ * εᵃ[x]
 
-    # Government spending share with persistent spending disturbance.
-    Gov[0] = Gov_y * Y[0] * exp(gov_gap[0])
+    # [4.26] Government spending level (level form from the log-linearized process).
+    Gov[0] = Govʸ * Y[0] * exp(gov_gap[0])
 
-    # Government spending disturbance.
-    gov_gap[0] = ρ_G * gov_gap[-1] + std_G * eps_G[x]
+    # [4.26] Government spending AR(1) disturbance.
+    gov_gap[0] = ρᵍ * gov_gap[-1] + σᵍ * εᵍ[x]
 
-    # Money demand equation included in the baseline quantitative model.
-    M_real[0] = b_m * C[0] / (R[0] - 1)
+    # [B.5] Money demand equation.
+    M_real[0] = χ * C[0] * R[0] / (R[0] - 1)
 
-    # Entrepreneurial net worth before consumption, net of lender monitoring share Γ.
-    V[0] = (1 - Gamma[0]) * Rk[0] * Q[-1] * K[-1]
+    # [4.8] Entrepreneurial equity (net of lender repayment and monitoring).
+    V[0] = (1 - Γ[0]) * Rᵏ[0] * Q[-1] * K[-1]
 
-    # Entrepreneurial consumption rule with survival probability γ_e.
-    Ce[0] = (1 - γ_e) * V[0]
+    # [4.8] Entrepreneurial consumption: dying entrepreneurs consume their equity.
+    Cᵉ[0] = (1 - γᵉ) * V[0]
 
-    # Entrepreneurial net worth: retained entrepreneurial wealth plus labor income.
-    N[0] = γ_e * V[0] + W_e[0]
+    # [4.7] Entrepreneurial net worth: retained equity plus labor income.
+    N[0] = γᵉ * V[0] + Wᵉ[0]
 
-    # Balance sheet identity defining leverage.
+    # [3.2] Balance sheet identity defining leverage.
     leverage[0] * N[0] = Q[0] * K[0]
 
-    # Appendix B lognormal distribution: default probability F(ωbar).
-    F[0] = normcdf((log(omega_bar[0]) + σ_ω^2 / 2) / σ_ω)
+    # [A.2] Lognormal distribution: default probability F(ω̄).
+    F[0] = normcdf((log(ω̄[0]) + σ_ω^2 / 2) / σ_ω)
 
-    # Appendix B lognormal distribution: partial expectation G(ωbar).
-    G[0] = normcdf((log(omega_bar[0]) - σ_ω^2 / 2) / σ_ω)
+    # [A.2] Lognormal distribution: partial expectation G(ω̄).
+    G[0] = normcdf((log(ω̄[0]) - σ_ω^2 / 2) / σ_ω)
 
-    # Appendix B contract object Γ(ωbar) = G(ωbar) + ωbar * (1 - F(ωbar)).
-    Gamma[0] = G[0] + omega_bar[0] * (1 - F[0])
+    # [A.2] Contract object Γ(ω̄) = G(ω̄) + ω̄(1 - F(ω̄)).
+    Γ[0] = G[0] + ω̄[0] * (1 - F[0])
 
-    # Lender participation condition solved for the external finance premium.
-    finance_premium[0] = (1 - 1 / leverage[0]) / (Gamma[0] - μ * G[0])
+    # [3.5] Lender participation condition (inverted for external finance premium s).
+    s[0] = (1 - 1 / leverage[0]) / (Γ[0] - μ * G[0])
 
-    # Optimal contract FOC linking leverage and the default threshold.
-    leverage[0] - 1 = (1 - F[0]) * (Gamma[0] - μ * G[0]) / ((1 - F[0] - μ * omega_bar[0] * normpdf((log(omega_bar[0]) + σ_ω^2 / 2) / σ_ω) / σ_ω) * (1 - Gamma[0]))
+    # [A.1]–[A.2] Optimal contract FOC linking leverage and the default threshold.
+    leverage[0] - 1 = (1 - F[0]) * (Γ[0] - μ * G[0]) / ((1 - F[0] - μ * ω̄[0] * normpdf((log(ω̄[0]) + σ_ω^2 / 2) / σ_ω) / σ_ω) * (1 - Γ[0]))
 
-    # External finance premium wedge: expected capital return over the safe real rate.
-    Rk[1] = finance_premium[0] * R[0] / Pi[1]
+    # [4.5] External finance premium wedge (ex-post).
+    Rᵏ[1] = s[0] * R[0] / π[1]
 
-    # Aggregate monitoring costs paid in default states.
-    monitoring_cost[0] = μ * G[0] * Rk[0] * Q[-1] * K[-1]
+    # [B.8] Aggregate monitoring costs paid in default states.
+    monitoring_cost[0] = μ * G[0] * Rᵏ[0] * Q[-1] * K[-1]
 
     # Observable/log-output convenience variable.
     log_y[0] = log(Y[0])
 
     # Annualized external finance premium convenience variable.
-    premium_ann[0] = 400 * log(finance_premium[0])
+    premium_ann[0] = 400 * log(s[0])
 end
 
 @parameters Bernanke_Gertler_Gilchrist_1999_nonlinear begin
@@ -103,22 +103,23 @@ end
     α = 0.35
     δ = 0.025
     Ω = 0.015384615384615385
+    Hᵉ = 1.0
     ζ = 2.0
-    ψ_i = 4.0
+    ψⁱ = 4.0
     μ = 0.12
     σ_ω = 0.28
-    γ_e = 0.9728
-    Pi_ss = 1.0
+    γᵉ = 0.9728
+    πˢˢ = 1.0
     θ = 0.75
-    ϵ_p = 11.0
-    ρ_R = 0.9
+    ϵᵖ = 11.0
+    ρʳ = 0.9
     ϕ_pi = 1.1
-    ϕ_y = 0.0
-    ρ_A = 0.95
-    std_A = 0.01
-    Gov_y = 0.20
-    ρ_G = 0.95
-    std_G = 0.01
-    std_R = 0.0025
-    b_m = 0.01
+    ϕʸ = 0.0
+    ρᵃ = 0.95
+    σᵃ = 0.01
+    Govʸ = 0.20
+    ρᵍ = 0.95
+    σᵍ = 0.01
+    σʳ = 0.0025
+    χ = 0.0099
 end
--- models/Gertler_Karadi_2011_nonlinear.jl
+++ models/Gertler_Karadi_2011_nonlinear.jl
@@ -1,290 +1,124 @@
 using MacroModelling
 
-# function GK2011_intermediary_leverage(spread, β, θ, λ)
-#     aa = λ * β * θ * spread
-#     bb = -(1 - θ) * (λ - β * spread)
-#     cc = 1 - θ
-#     discriminant = bb^2 - 4 * aa * cc
-
-#     if discriminant < 0
-#         return NaN
-#     end
-
-#     return (-bb - sqrt(discriminant)) / (2 * aa)
-# end
-
-# function GK2011_spread_gap(spread, β, θ, λ, ω)
-#     R = 1 / β
-#     φ = GK2011_intermediary_leverage(spread, β, θ, λ)
-#     z = spread * φ + R
-
-#     return φ * ω / (1 - θ * z) - 1
-# end
-
-# function GK2011_solve_spread(β, θ, λ, ω)
-#     lower = 1e-8
-#     lower_gap = GK2011_spread_gap(lower, β, θ, λ, ω)
-
-#     upper = lower
-#     upper_gap = lower_gap
-#     for trial in exp.(range(log(lower * 1.01), log(0.05), length = 400))
-#         trial_gap = GK2011_spread_gap(trial, β, θ, λ, ω)
-#         if isfinite(trial_gap)
-#             upper = trial
-#             upper_gap = trial_gap
-#             if lower_gap * upper_gap <= 0
-#                 break
-#             end
-#         end
-#     end
-
-#     if lower_gap * upper_gap > 0 || !isfinite(upper_gap)
-#         error("Could not bracket the GK2011 steady-state intermediary spread.")
-#     end
-
-#     midpoint = (lower + upper) / 2
-#     for iteration in 1:200
-#         midpoint = (lower + upper) / 2
-#         midpoint_gap = GK2011_spread_gap(midpoint, β, θ, λ, ω)
-
-#         if abs(midpoint_gap) < 1e-13
-#             return midpoint
-#         elseif lower_gap * midpoint_gap <= 0
-#             upper = midpoint
-#             upper_gap = midpoint_gap
-#         else
-#             lower = midpoint
-#             lower_gap = midpoint_gap
-#         end
-#     end
-
-#     return midpoint
-# end
-
-# function GK2011_nonlinear_steady_state!(ss, parameters)
-#     β, σ, h, χ, φ_l, ζ_u, θ, α, δ, g_y, η_i, ϵ_p, γ_p, γ_p_index, ρ_i,
-#     κ_pi, ρ_ξ, std_ξ, ρ_A, std_A, ρ_g, std_g, std_Ne, std_i, λ, ω, b_u,
-#     κ_y = parameters
-
-#     Pm = (ϵ_p - 1) / ϵ_p
-#     X = 1 / Pm
-#     R = 1 / β
-#     spread = GK2011_solve_spread(β, θ, λ, ω)
-#     Rk = R + spread
-#     φ = GK2011_intermediary_leverage(spread, β, θ, λ)
-#     z = spread * φ + R
-#     x = z
-#     ν = ((1 - θ) * β * spread) / (1 - β * θ * x)
-#     η = (1 - θ) / (1 - β * θ * z)
-
-#     K_to_L = (Pm * α / (Rk - 1 + δ))^(1 / (1 - α))
-#     consumption_to_labor = (1 - g_y) * K_to_L^α - δ * K_to_L
-#     labor_constant =
-#         (1 - β * h) * ((1 - h) * consumption_to_labor)^(-σ) *
-#         Pm * (1 - α) * K_to_L^α
-#     L = (labor_constant / χ)^(1 / (φ_l + σ))
-#     K = K_to_L * L
-#     Y = K^α * L^(1 - α)
-#     Ym = Y
-#     I = δ * K
-#     G = g_y * Y
-#     C = Y - I - G
-#     varrho = (1 - β * h) * ((1 - h) * C)^(-σ)
-#     Λ = 1.0
-#     Q = 1.0
-#     Ξ = 1.0
-#     U = 1.0
-#     δ_rate = δ
-#     In = 0.0
-#     N = K / φ
-#     Ne = θ * z * N
-#     Nn = ω * K
-#     Keff = K
-#     w = Pm * (1 - α) * Y / L
-#     VMPK = Pm * α * Y / K
-#     D = 1.0
-#     F_price = Y * Pm / (1 - β * γ_p)
-#     Z_price = Y / (1 - β * γ_p)
-#     Pi = 1.0
-#     Pi_star = 1.0
-#     i_nom = R
-#     prem = Rk / R
-#     A = 1.0
-#     g_gap = 0.0
-
-#     if length(ss) != 38
-#         resize!(ss, 38)
-#     end
-
-#     ss[1] = A
-#     ss[2] = C
-#     ss[3] = D
-#     ss[4] = F_price
-#     ss[5] = G
-#     ss[6] = I
-#     ss[7] = In
-#     ss[8] = K
-#     ss[9] = Keff
-#     ss[10] = L
-#     ss[11] = Λ
-#     ss[12] = N
-#     ss[13] = Ne
-#     ss[14] = Nn
-#     ss[15] = Pi
-#     ss[16] = Pi_star
-#     ss[17] = Pm
-#     ss[18] = Q
-#     ss[19] = R
-#     ss[20] = Rk
-#     ss[21] = U
-#     ss[22] = VMPK
-#     ss[23] = X
-#     ss[24] = Ξ
-#     ss[25] = Y
-#     ss[26] = Ym
-#     ss[27] = Z_price
-#     ss[28] = δ_rate
-#     ss[29] = η
-#     ss[30] = g_gap
-#     ss[31] = i_nom
-#     ss[32] = ν
-#     ss[33] = φ
-#     ss[34] = prem
-#     ss[35] = varrho
-#     ss[36] = w
-#     ss[37] = x
-#     ss[38] = z
-
-#     return ss
-# end
-
-# function GK2011_nonlinear_steady_state(parameters)
-#     ss = Vector{Float64}(undef, 38)
-#     return GK2011_nonlinear_steady_state!(ss, parameters)
-# end
-
-
 @model Gertler_Karadi_2011_nonlinear begin
-    # GK replication eq. 1: marginal utility of consumption with external habit.
-    varrho[0] = (C[0] - h * C[-1])^(-σ) - β * h * (C[1] - h * C[0])^(-σ)
+    # [4] Marginal utility of consumption with external habit.
+    ϱ[0] = (C[0] - h * C[-1])^(-σ) - β * h * (C[1] - h * C[0])^(-σ)
 
-    # GK replication eq. 2: household Euler equation for the riskless asset.
-    β * R[0] * Lambda[1] = 1.0
+    # [4'] Household Euler equation for the riskless asset.
+    β * R[0] * Λ[1] = 1.0
 
-    # GK replication eq. 3: stochastic discount factor.
-    Lambda[0] = varrho[0] / varrho[-1]
+    # [--] Stochastic discount factor.
+    Λ[0] = ϱ[0] / ϱ[-1]
 
-    # GK replication eq. 4: labor market equilibrium.
-    χ * L[0]^φ_l = varrho[0] * Pm[0] * (1 - α) * Y[0] / L[0]
+    # [3] Labor market equilibrium.
+    χ * L[0]^φ_l = ϱ[0] * Pₘ[0] * (1 - α) * Y[0] / L[0]
 
-    # GK replication eq. 5: marginal value of bankers' capital.
-    nu[0] = (1 - θ) * β * Lambda[1] * (Rk[1] - R[0]) + β * Lambda[1] * θ * x[1] * nu[1]
+    # [11] Marginal value of bankers' capital (ν_t).
+    ν[0] = (1 - θ) * β * Λ[1] * (Rᵏ[1] - R[0]) + β * Λ[1] * θ * x[1] * ν[1]
 
-    # GK replication eq. 6: marginal value of bankers' net worth.
-    eta[0] = (1 - θ) + β * Lambda[1] * θ * z[1] * eta[1]
+    # [11] Marginal value of bankers' net worth (η_t).
+    η[0] = (1 - θ) + β * Λ[1] * θ * z[1] * η[1]
 
-    # GK replication eq. 7: incentive-constraint leverage relation.
-    phi[0] = eta[0] / (λ - nu[0])
+    # [13] Incentive-constraint leverage relation (φ_t = η_t/(λ - ν_t)).
+    φ[0] = η[0] / (λ - ν[0])
 
-    # GK replication eq. 8: gross growth rate of bankers' capital.
-    z[0] = (Rk[0] - R[-1]) * phi[-1] + R[-1]
+    # [14] Gross growth rate of bankers' capital (z_{t-1,t} = N_t/N_{t-1}).
+    z[0] = (Rᵏ[0] - R[-1]) * φ[-1] + R[-1]
 
-    # GK replication eq. 9: gross growth rate of bankers' net worth.
-    x[0] = phi[0] / phi[-1] * z[0]
+    # [--] Gross growth rate of assets (x_{t-1,t} = Q_t S_t/(Q_{t-1} S_{t-1})).
+    x[0] = φ[0] / φ[-1] * z[0]
 
-    # GK replication eq. 10: aggregate intermediary balance sheet.
-    Q[0] * K[0] = phi[0] * N[0]
+    # [15] Aggregate intermediary balance sheet.
+    Q[0] * K[0] = φ[0] * N[0]
 
-    # GK replication eq. 11: aggregate bankers' net worth.
-    N[0] = Ne[0] + Nn[0]
+    # [16] Aggregate bankers' net worth (survivors + entrants).
+    N[0] = Nᵉ[0] + Nⁿ[0]
 
-    # GK replication eq. 12: existing bankers' net worth accumulation.
-    Ne[0] = θ * z[0] * N[-1] * exp(-std_Ne * eps_Ne[x])
+    # [17] Existing bankers' net worth accumulation.
+    Nᵉ[0] = θ * z[0] * N[-1] * exp(-σ_Ne * ε_Ne[x])
 
-    # GK replication eq. 13: entering bankers' net worth.
-    Nn[0] = ω * Q[0] * Xi[0] * K[-1]
+    # [18] Entering bankers' net worth.
+    Nⁿ[0] = ω * Q[0] * ξ[0] * K[-1]
 
-    # GK replication eq. 14: gross return on capital.
-    Rk[0] = (Pm[0] * α * Ym[0] / K[-1] + Xi[0] * (Q[0] - delta_rate[0])) / Q[-1]
+    # [25] Gross return on capital.
+    Rᵏ[0] = (Pₘ[0] * α * Ym[0] / K[-1] + ξ[0] * (Q[0] - δ_rate[0])) / Q[-1]
 
-    # GK replication eq. 15: intermediate goods production.
-    Ym[0] = A[0] * (Xi[0] * U[0] * K[-1])^α * L[0]^(1 - α)
+    # [22] Intermediate goods production.
+    Ym[0] = A[0] * (ξ[0] * U[0] * K[-1])^α * L[0]^(1 - α)
 
-    # GK replication eq. 16: capital producer's optimal investment condition.
+    # [27] Capital producer's optimal investment condition (Tobin's Q).
     Q[0] = 1 + η_i / 2 * ((In[0] + I[ss]) / (In[-1] + I[ss]) - 1)^2 +
         η_i * ((In[0] + I[ss]) / (In[-1] + I[ss]) - 1) * (In[0] + I[ss]) / (In[-1] + I[ss]) -
-        β * Lambda[1] * η_i * ((In[1] + I[ss]) / (In[0] + I[ss]) - 1) * ((In[1] + I[ss]) / (In[0] + I[ss]))^2
+        β * Λ[1] * η_i * ((In[1] + I[ss]) / (In[0] + I[ss]) - 1) * ((In[1] + I[ss]) / (In[0] + I[ss]))^2
 
-    # GK replication eq. 17: utilization-dependent depreciation.
-    delta_rate[0] = δ + b_u / (1 + ζ_u) * (U[0]^(1 + ζ_u) - 1)
+    # [--] Utilization-dependent depreciation.
+    δ_rate[0] = δ + b_u / (1 + ζ_u) * (U[0]^(1 + ζ_u) - 1)
 
-    # GK replication eq. 18: optimal capacity utilization.
-    Pm[0] * α * Ym[0] / U[0] = b_u * U[0]^ζ_u * Xi[0] * K[-1]
+    # [23] Optimal capacity utilization.
+    Pₘ[0] * α * Ym[0] / U[0] = b_u * U[0]^ζ_u * ξ[0] * K[-1]
 
-    # GK replication eq. 19: net investment.
-    In[0] = I[0] - delta_rate[0] * Xi[0] * K[-1]
+    # [26] Net investment.
+    In[0] = I[0] - δ_rate[0] * ξ[0] * K[-1]
 
-    # GK replication eq. 20: capital accumulation.
-    K[0] = Xi[0] * K[-1] + In[0]
+    # [35] Capital accumulation.
+    K[0] = ξ[0] * K[-1] + In[0]
 
-    # GK replication eq. 21: government consumption.
-    G[0] = g_y * Y[ss] * exp(g_gap[0])
+    # [--] Government consumption.
+    G[0] = g_y * Y[ss] * exp(ĝ[0])
 
-    # GK replication eq. 22: aggregate resource constraint.
+    # [34] Aggregate resource constraint.
     Y[0] = C[0] + G[0] + I[0] + η_i / 2 * ((In[0] + I[ss]) / (In[-1] + I[ss]) - 1)^2 * (In[0] + I[ss])
 
-    # GK replication eq. 23: wholesale and retail output relation.
+    # [--] Wholesale and retail output relation.
     Ym[0] = Y[0] * D[0]
 
-    # GK replication eq. 24: Calvo price dispersion.
-    D[0] = γ_p * D[-1] * Pi[-1]^(-γ_p_index * ϵ_p) * Pi[0]^ϵ_p +
-        (1 - γ_p) * ((1 - γ_p * Pi[-1]^(γ_p_index * (1 - γ_p)) * Pi[0]^(γ_p - 1)) / (1 - γ_p))^(-ϵ_p / (1 - γ_p))
+    # [--] Calvo price dispersion.
+    D[0] = γ_p * D[-1] * π[-1]^(-γ_p_index * ϵ_p) * π[0]^ϵ_p +
+        (1 - γ_p) * ((1 - γ_p * π[-1]^(γ_p_index * (1 - ϵ_p)) * π[0]^(ϵ_p - 1)) / (1 - γ_p))^(-ϵ_p / (1 - ϵ_p))
 
-    # GK replication eq. 25: markup definition.
-    X[0] = 1 / Pm[0]
+    # [--] Markup definition.
+    X[0] = 1 / Pₘ[0]
 
-    # GK replication eq. 26: optimal reset-price numerator recursion.
-    F_price[0] = Y[0] * Pm[0] + β * γ_p * Lambda[1] * Pi[1]^ϵ_p * Pi[0]^(-ϵ_p * γ_p_index) * F_price[1]
+    # [31] Optimal reset-price numerator recursion (F_t).
+    F[0] = Y[0] * Pₘ[0] + β * γ_p * Λ[1] * π[1]^ϵ_p * π[0]^(-ϵ_p * γ_p_index) * F[1]
 
-    # GK replication eq. 27: optimal reset-price denominator recursion.
-    Z_price[0] = Y[0] + β * γ_p * Lambda[1] * Pi[1]^(ϵ_p - 1) * Pi[0]^(γ_p_index * (1 - ϵ_p)) * Z_price[1]
+    # [31] Optimal reset-price denominator recursion (Z_t).
+    Z[0] = Y[0] + β * γ_p * Λ[1] * π[1]^(ϵ_p - 1) * π[0]^(γ_p_index * (1 - ϵ_p)) * Z[1]
 
-    # GK replication eq. 28: optimal reset-price inflation.
-    Pi_star[0] = ϵ_p / (ϵ_p - 1) * F_price[0] / Z_price[0] * Pi[0]
+    # [31]–[32] Optimal reset-price inflation.
+    π_star[0] = ϵ_p / (ϵ_p - 1) * F[0] / Z[0] * π[0]
 
-    # GK replication eq. 29: aggregate price index.
-    Pi[0]^(1 - ϵ_p) = γ_p * Pi[-1]^(γ_p_index * (1 - ϵ_p)) + (1 - γ_p) * Pi_star[0]^(1 - ϵ_p)
+    # [33] Aggregate price index.
+    π[0]^(1 - ϵ_p) = γ_p * π[-1]^(γ_p_index * (1 - ϵ_p)) + (1 - γ_p) * π_star[0]^(1 - ϵ_p)
 
-    # GK replication eq. 30: Fisher equation.
-    i_nom[0] = R[0] * Pi[1]
+    # [38] Fisher equation.
+    i[0] = R[0] * π[1]
 
-    # GK replication eq. 31: interest-rate rule.
-    i_nom[0] = i_nom[-1]^ρ_i * ((1 / β) * Pi[0]^κ_pi * (X[0] / (ϵ_p / (ϵ_p - 1)))^κ_y)^(1 - ρ_i) * exp(std_i * eps_i[x])
+    # [37] Interest-rate rule (Taylor rule with smoothing).
+    i[0] = i[-1]^ρ_i * ((1 / β) * π[0]^κ_pi * (X[0] / (ϵ_p / (ϵ_p - 1)))^κ_y)^(1 - ρ_i) * exp(σ_i * ε_i[x])
 
-    # GK replication eq. 32: TFP process.
-    A[0] = A[-1]^ρ_A * exp(-std_A * eps_A[x])
+    # [--] TFP process.
+    A[0] = A[-1]^ρ_A * exp(-σ_A * ε_A[x])
 
-    # GK replication eq. 33: capital quality process.
-    Xi[0] = Xi[-1]^ρ_ξ * exp(-std_ξ * eps_ξ[x])
+    # [--] Capital quality process.
+    ξ[0] = ξ[-1]^ρ_ξ * exp(-σ_ξ * ε_ξ[x])
 
-    # GK replication eq. 34: government spending process.
-    g_gap[0] = ρ_g * g_gap[-1] - std_g * eps_g[x]
+    # [--] Government spending process.
+    ĝ[0] = ρ_g * ĝ[-1] - σ_g * ε_g[x]
 
-    # GK replication eq. 35: effective capital convenience variable.
-    Keff[0] = Xi[0] * K[-1]
+    # [--] Effective capital convenience variable.
+    Keff[0] = ξ[0] * K[-1]
 
-    # GK replication eq. 36: wage convenience variable.
-    w[0] = Pm[0] * (1 - α) * Y[0] / L[0]
+    # [--] Wage convenience variable.
+    w[0] = Pₘ[0] * (1 - α) * Y[0] / L[0]
 
-    # GK replication eq. 37: marginal value product of capital convenience variable.
-    VMPK[0] = Pm[0] * α * Y[0] / (Xi[0] * K[-1])
+    # [--] Marginal value product of capital convenience variable.
+    VMPK[0] = Pₘ[0] * α * Y[0] / (ξ[0] * K[-1])
 
-    # GK replication eq. 39: premium convenience variable.
-    prem[0] = Rk[1] / R[0]
+    # [--] Premium convenience variable (Rᵏ_{t+1}/R_{t+1}).
+    prem[0] = Rᵏ[1] / R[0]
 end
 
-
-# @parameters Gertler_Karadi_2011_nonlinear steady_state_function = GK2011_nonlinear_steady_state! begin
 @parameters Gertler_Karadi_2011_nonlinear begin
     β = 0.99
     σ = 1.0
@@ -300,46 +134,18 @@ end
     ϵ_p = 4.167
     γ_p = 0.779
     γ_p_index = 0.241
-    ρ_i = 0.0
+    ρ_i = 0.8
     κ_pi = 1.5
     κ_y = -0.5 / 4
     ρ_ξ = 0.66
-    std_ξ = 0.05
+    σ_ξ = 0.05
     ρ_A = 0.95
-    std_A = 0.01
+    σ_A = 0.01
     ρ_g = 0.95
-    std_g = 0.01
-    std_Ne = 0.01
-    std_i = 0.01
+    σ_g = 0.01
+    σ_Ne = 0.01
+    σ_i = 0.01
     λ = 0.38149498593241726
     ω = 0.0022277804179292593
     b_u = 0.037601010101010155
-
-    β > 0
-    σ > 0
-    0 <= h < 1
-    χ > 0
-    φ_l > 0
-    ζ_u > 0
-    0 < θ < 1
-    0 < α < 1
-    0 < δ < 1
-    0 <= g_y < 1
-    η_i >= 0
-    ϵ_p > 1
-    0 <= γ_p < 1
-    0 <= γ_p_index < 1
-    0 <= ρ_i < 1
-    κ_pi > 1
-    0 <= ρ_ξ < 1
-    std_ξ >= 0
-    0 <= ρ_A < 1
-    std_A >= 0
-    0 <= ρ_g < 1
-    std_g >= 0
-    std_Ne >= 0
-    std_i >= 0
-    λ > 0
-    ω > 0
-    b_u > 0
 end
